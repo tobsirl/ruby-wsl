87b595456f45ecb4c0ddb8e4001b1376b4f6e7a3
Paul Tobin <tobster111@gmail.com>
2022-09-29 18:37:21 +0100

    solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -37,3 +37,6 @@ end
 # Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
 # Return your answer as a number.
+def sum_mix(x)
+  x.map { |v| v.to_i }.reduce(:+)
+end
