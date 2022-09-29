4eeb933296af2269af90806fabec5e92153adfff
Paul Tobin <tobster111@gmail.com>
2022-09-29 18:36:09 +0100

    code setup

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -32,3 +32,8 @@ def rps(p1, p2)
     "Player 2 won!"
   end
 end
+
+# Sum Mixed Array 8kyu
+# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+# Return your answer as a number.
