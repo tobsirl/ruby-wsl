e6a52f9a6c476f8e439a32f249207a7ab68bfa04
Paul Tobin <tobster111@gmail.com>
2022-10-03 15:48:21 +0100

    pangram

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -40,3 +40,14 @@ end
 def sum_mix(x)
   x.map { |v| v.to_i }.reduce(:+)
 end
+
+# Detect Pangram 6kyu
+# A pangram is a sentence that contains every single letter of the alphabet at least once. 
+# For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+# because it uses the letters A-Z at least once (case is irrelevant).
+
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+def panagram?(string)
+  string.downcase.scan(/[a-z]/).uniq.size == 26
+end
