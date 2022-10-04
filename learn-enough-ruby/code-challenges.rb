4ff93843738f1cd607ec41371e26b98df32260a3
Paul Tobin <tobster111@gmail.com>
2022-10-04 11:48:00 +0100

    code challenge

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -55,3 +55,14 @@ end
 def panagram?(string)
   ('a'..'z').all? { |x| string.downcase.include? (x) } 
 end
+
+# Complementary DNA 7kyu
+# Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for 
+# the development and functioning of living organisms.
+
+# If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+# In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+# Your function receives one side of the DNA (string, except for Haskell); 
+# you need to return the other complementary side. 
+# DNA strand is never empty or there is no DNA at all (again, except for Haskell).
