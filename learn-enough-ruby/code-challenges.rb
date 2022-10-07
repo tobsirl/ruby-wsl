45e8c7b6fc9e0307b02443f99411aaa3c9ad070e
Paul Tobin <tobster111@gmail.com>
2022-10-07 10:38:46 +0100

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -82,3 +82,6 @@ end
 # Sum of two lowest positive integers 7kyu
 # Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
 # No floats or non-positive integers will be passed.
+def sum_two_smallest_numbers(numbers)
+  numbers.sort[0] + numbers.sort[1]
+end
