3618c0b58be263ca8fb7e964d81dbb79b926f833
Paul Tobin <tobster111@gmail.com>
2022-10-09 11:22:23 +0100

    code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -119,3 +119,15 @@ end
 def find_short(s)
   return s.split(' ').map(&:length).min
 end
+
+# Hello, Name or World! 8kyu
+# Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+# Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+def hello(name = "")
+  if name.empty?
+    "Hello, World!"
+  else  
+    "Hello, #{name.downcase.capitalize}!"
+  end
+end
