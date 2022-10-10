fb7cf24df66df95ce7e485d7c119c915d5207e15
Paul Tobin <tobster111@gmail.com>
2022-10-10 19:02:20 +0100

    code challenge best

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -156,3 +156,10 @@ def final_grade(exam, projects)
     0
   end
 end
+
+def final_grade(exam, projects)
+  return 100 if exam > 90 || projects > 10
+  return 90 if exam > 75 && projects >= 5
+  return 75 if exam > 50 && projects >= 2
+  0
+end
