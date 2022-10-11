f4b2048cc914bca755ddd41f6bd4f27cc7a0fdfa
Paul Tobin <tobster111@gmail.com>
2022-10-11 18:47:19 +0100

    challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -177,4 +177,14 @@ end
 # If you can't sleep, just count sheep!!
 # Task:
 # Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
-
+def count_sheep(num)
+  if num == 0
+    return ""
+  end
+  
+  arr = []
+  (1..num).each do |i|
+    arr.push("#{i} sheep...")
+  end
+  arr.join
+end
