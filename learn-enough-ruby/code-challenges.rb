d4464923923ff7f0bac1be7d01dfd044410ed8fa
Paul Tobin <tobster111@gmail.com>
2022-10-11 18:46:53 +0100

    challenge

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -172,3 +172,9 @@ def final_grade(exam, projects)
   else 0
   end
 end
+
+# If you can't sleep, just count sheep!! 8kyu
+# If you can't sleep, just count sheep!!
+# Task:
+# Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
