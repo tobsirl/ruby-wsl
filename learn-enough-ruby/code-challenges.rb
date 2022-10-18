fce8db9e5c13abf0685e0da61020d210612df006
Paul Tobin <tobster111@gmail.com>
2022-10-18 18:29:12 +0100

    Coding challenge

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -213,3 +213,9 @@ end
 def duty_free(price, discount, holiday_cost)
   ( holiday_cost/(price*discount/100.0) ).floor
 end
+
+# All Star Code Challenge #18 8kyu
+# Create a function that accepts 2 string arguments and returns an integer of the count of 
+# occurrences the 2nd argument is found in the first one.
+
+# If no occurrences can be found, a count of 0 should be returned.
