be87a866b009f1f5e61bf73fe317e3c2ec37ec2d
Paul Tobin <tobster111@gmail.com>
2022-10-19 17:35:20 +0100

    challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -232,3 +232,8 @@ end
 
 # Your task is to convert strings to how they would be written by Jaden Smith. 
 # The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+class String
+  def toJadenCase
+    self.split.map { |word| word.capitalize }.join(" ")
+  end
+end
