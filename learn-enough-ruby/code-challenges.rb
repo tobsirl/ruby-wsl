2cdf1088705a3dc00bc99bd3e40710d4993d5262
Paul Tobin <tobster111@gmail.com>
2022-10-21 14:08:49 +0100

    challenge

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -260,3 +260,12 @@ end
 def between(a, b)
   [*a..b]
 end
+
+# L1: Set Alarm 8kyu
+# Write a function named setAlarm which receives two parameters. 
+# The first parameter, employed, is true whenever you are employed and the second parameter, 
+# vacation is true whenever you are on vacation.
+
+# The function should return true if you are employed and not on vacation 
+# (because these are the circumstances under which you need to set an alarm). 
+# It should return false otherwise.
