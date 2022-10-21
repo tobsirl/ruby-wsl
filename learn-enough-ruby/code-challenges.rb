eac439536e10e26bfe52ac5ec1adaa8313fded03
Paul Tobin <tobster111@gmail.com>
2022-10-21 14:09:19 +0100

    challenge solutoin

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -269,3 +269,6 @@ end
 # The function should return true if you are employed and not on vacation 
 # (because these are the circumstances under which you need to set an alarm). 
 # It should return false otherwise.
+def set_alarm(employed, vacation)
+  employed && !vacation 
+end
