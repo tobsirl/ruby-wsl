0f31e2ffb8912a727bd63fcd2ad7312dee23cea4
Paul Tobin <tobster111@gmail.com>
2022-10-27 18:38:00 +0100

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -298,3 +298,13 @@ end
 # The manager of the car rental makes you some good offers.
 # Every day you rent the car costs $40. If you rent the car for 7 or more days, 
 # you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+def rental_car_cost(d)
+  total = d * 40
+    if d >= 7 
+      total - 50
+    elsif d >= 3
+      total - 20
+    else
+      d * 40
+    end
+end
