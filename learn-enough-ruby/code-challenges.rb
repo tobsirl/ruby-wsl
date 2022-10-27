58c206e92f78c4c00be8624882e1d9fc15a1a7d2
Paul Tobin <tobster111@gmail.com>
2022-10-27 18:38:49 +0100

    Code challenge variations

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -308,3 +308,16 @@ def rental_car_cost(d)
       d * 40
     end
 end
+
+def rental_car_cost(d)
+  return d * 40 if d < 3
+  return d * 40 - 20 if d < 7
+  return d * 40 - 50
+end
+
+def rental_car_cost(days)
+  discount = 0
+  discount = 20 if days >= 3
+  discount = 50 if days >= 7
+  days * 40 - discount
+end
