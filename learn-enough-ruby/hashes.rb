54e35dc1c0e989b82017057ead3a96a7c05a90bc
Paul Tobin <tobster111@gmail.com>
2022-10-29 13:20:44 +0100

    Hashes what there are

--- learn-enough-ruby/hashes.rb
+++ learn-enough-ruby/hashes.rb
@@ -1,3 +1,6 @@
 # simple_hash = { "a" => 100, "b" => 200 }
 # my_details = { 'name' => 'paul', 'favcolor' => 'red' }
 # my_details["favcolor"]
+
+# Hashes are also called associative arrays or dictionaries in other languages.
+# Hash a.k.a dictionary
