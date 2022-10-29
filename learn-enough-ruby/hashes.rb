c0a06ece48471473c986da544287380c0d3295ee
Paul Tobin <tobster111@gmail.com>
2022-10-29 17:22:36 +0100

    Hashes adding to a hash

--- learn-enough-ruby/hashes.rb
+++ learn-enough-ruby/hashes.rb
@@ -1,20 +1,23 @@
 simple_hash = { "a" => ["100", "150"], "b" => 200, "c" => 300 }
 my_details = { :name => 'paul', :favcolor => 'red' }
 another_hash = { a: 100, b: 200, c: 300 }
-puts my_details["favcolor"] # red
+# puts my_details["favcolor"] # red
 
 # Hashes are also called associative arrays or dictionaries in other languages.
 # Hash a.k.a dictionary
-puts simple_hash["a"] # => 100
-puts another_hash # => 100
-puts another_hash.keys
-puts another_hash.values
+# puts simple_hash["a"] # => 100
+# puts another_hash # => 100
+# puts another_hash.keys
+# puts another_hash.values
 
-another_hash.each do |key, value|
-  puts "The class for key is #{key.class} and the value is #{value.class}"
-end
+# another_hash.each do |key, value|
+#   puts "The class for key is #{key.class} and the value is #{value.class}"
+# end
 
-my_details.each do |key, value|
-  puts "The class for key is #{key.class} and the value is #{value.class}"
-end
+# my_details.each do |key, value|
+#   puts "The class for key is #{key.class} and the value is #{value.class}"
+# end
 
+hash = { :a => 1, :b => 2, :c => 3, :d => 4 }
+hash[:e] = "f"
+puts hash
