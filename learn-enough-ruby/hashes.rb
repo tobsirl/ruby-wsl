63a562c887eb134479daa582e26dcb29df883e36
Paul Tobin <tobster111@gmail.com>
2022-10-29 13:26:34 +0100

    Hashes accessing values

--- learn-enough-ruby/hashes.rb
+++ learn-enough-ruby/hashes.rb
@@ -1,6 +1,7 @@
-# simple_hash = { "a" => 100, "b" => 200 }
-# my_details = { 'name' => 'paul', 'favcolor' => 'red' }
-# my_details["favcolor"]
+simple_hash = { "a" => 100, "b" => 200, "c" => 300 }
+my_details = { 'name' => 'paul', 'favcolor' => 'red' }
+puts my_details["favcolor"] # red
 
 # Hashes are also called associative arrays or dictionaries in other languages.
 # Hash a.k.a dictionary
+puts simple_hash["a"] # => 100
