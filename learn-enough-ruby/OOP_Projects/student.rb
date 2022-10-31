1ee63f3bba3a1b4f338f39199175e223659207a2
Paul Tobin <tobster111@gmail.com>
2022-10-31 16:05:55 +0000

    OOP Student setter

--- learn-enough-ruby/OOP_Projects/student.rb
+++ learn-enough-ruby/OOP_Projects/student.rb
@@ -5,11 +5,18 @@ class Student
   @username
   @password
 
+  # setter method
+  def first_name=(name)
+    @first_name = name
+  end
+  
+
   def to_s
     "First name: #{@first_name}"
   end
 end
 
-puts student = Student.new
-# student.first_name = "Paul"
-# puts student
+puts paul = Student.new
+put paul
+paul.first_name = "Paul"
+puts paul
