f88f4b3787f80a61cdb45c319f38eccd749ade46
Paul Tobin <tobster111@gmail.com>
2022-11-02 11:50:55 +0000

    Final Project check password

--- learn-enough-ruby/final_ruby_project.rb
+++ learn-enough-ruby/final_ruby_project.rb
@@ -1,13 +1,9 @@
 require 'bcrypt'
 
-my_password = BCrypt::Password.create("my password")
-#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
 
-my_password.version              #=> "2a"
-my_password.cost                 #=> 12
-my_password == "my password"     #=> true
-my_password == "not my password" #=> false
-
-my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
-my_password == "my password"     #=> true
-my_password == "not my password" #=> false
+new_password = create_hash_digest("password1")
+puts new_password
+puts new_password == "password1"
