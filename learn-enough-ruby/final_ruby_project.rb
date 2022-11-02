e8d28f7d5f7051a795bcaa577814135e07e1c1fd
Paul Tobin <tobster111@gmail.com>
2022-11-02 12:06:20 +0000

    Final Project new passwords

--- learn-enough-ruby/final_ruby_project.rb
+++ learn-enough-ruby/final_ruby_project.rb
@@ -16,3 +16,11 @@ def verify_hash_digest(password)
   BCrypt::Password.new(password)
 end
 
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+puts create_secure_users(users)
