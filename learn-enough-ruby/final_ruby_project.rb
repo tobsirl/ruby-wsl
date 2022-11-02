cde9bb68be780e03812be3660307202369d64d34
Paul Tobin <tobster111@gmail.com>
2022-11-02 12:04:09 +0000

    Final Project users

--- learn-enough-ruby/final_ruby_project.rb
+++ learn-enough-ruby/final_ruby_project.rb
@@ -1,5 +1,13 @@
 require 'bcrypt'
 
+users = [
+  { username: "mashrur", password: "password1" },
+  { username: "jack", password: "password2" },
+  { username: "arya", password: "password3" },
+  { username: "jonshow", password: "password4" },
+  { username: "heisenberg", password: "password5" }
+]
+
 def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
