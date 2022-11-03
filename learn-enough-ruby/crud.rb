657f1f439d6ae675ee6df9c9abd28bd3a64883b2
Paul Tobin <tobster111@gmail.com>
2022-11-03 17:39:12 +0000

    Final Project Crud

--- learn-enough-ruby/crud.rb
+++ learn-enough-ruby/crud.rb
@@ -2,7 +2,7 @@ module Crud
   require 'bcrypt'
   puts "Module CRUD activated"
 
-  def create_hash_digest(password)
+  def Crud.create_hash_digest(password)
     BCrypt::Password.create(password)
   end
 
@@ -10,7 +10,7 @@ module Crud
     BCrypt::Password.new(password)
   end
 
-  def create_secure_users(list_of_users)
+  def Crud.create_secure_users(list_of_users)
     list_of_users.each do |user_record|
       user_record[:password] = create_hash_digest(user_record[:password])
     end
