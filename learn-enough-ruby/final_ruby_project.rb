a2aba351df25cfef1f3739ddac3058618c9634a0
Paul Tobin <tobster111@gmail.com>
2022-11-03 17:28:38 +0000

    Final Project authenticate_user

--- learn-enough-ruby/final_ruby_project.rb
+++ learn-enough-ruby/final_ruby_project.rb
@@ -24,3 +24,12 @@ def create_secure_users(list_of_users)
 end
 
 puts create_secure_users(users)
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+      return user_record
+    end
+  end
+  "Credentials were not correct"
+end
