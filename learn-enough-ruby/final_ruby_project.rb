4601f127448eb514d0bbfab7c3578562ace34c15
Paul Tobin <tobster111@gmail.com>
2022-11-03 17:33:34 +0000

    Final Project module

--- learn-enough-ruby/final_ruby_project.rb
+++ learn-enough-ruby/final_ruby_project.rb
@@ -1,38 +1,28 @@
-require 'bcrypt'
+module Crud
+  require 'bcrypt'
+  puts "Module CRUD activated"
 
-users = [
-  { username: "mashrur", password: "password1" },
-  { username: "jack", password: "password2" },
-  { username: "arya", password: "password3" },
-  { username: "jonshow", password: "password4" },
-  { username: "heisenberg", password: "password5" }
-]
-
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
-end
 
-new_users = create_secure_users(users)
-puts new_users
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
 
-def authenticate_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
-      return user_record
+  def create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
     end
+    list_of_users
   end
-  "Credentials were not correct"
-end
 
-puts authenticated_user = authenticate_user("heisenberg", "password5", new_users)
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials were not correct"
+  end
+end
