174f0c61bd1089a07fd0ed41b0c95f504925b59a
Paul Tobin <tobster111@gmail.com>
2022-11-03 17:42:42 +0000

    Final Project self

--- learn-enough-ruby/crud.rb
+++ learn-enough-ruby/crud.rb
@@ -6,7 +6,7 @@ module Crud
     BCrypt::Password.create(password)
   end
 
-  def verify_hash_digest(password)
+  def self.verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
 
@@ -17,7 +17,7 @@ module Crud
     list_of_users
   end
 
-  def authenticate_user(username, password, list_of_users)
+  def self.authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
       if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
         return user_record
