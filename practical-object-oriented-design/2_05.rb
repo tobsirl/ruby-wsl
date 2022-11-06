105a8819a4eebcfbb4746593bad100644220c21e
Paul Tobin <tobster111@gmail.com>
2022-11-06 20:05:35 +0000

    OOP bike gear_inches

--- practical-object-oriented-design/2_05.rb
+++ practical-object-oriented-design/2_05.rb
@@ -9,16 +9,23 @@ ratio = chainring / cog.to_f
 puts ratio # 1.1111111111111112
 
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
   def initialize(chainring, cog)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
   
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
+  
 end
 
 puts Gear.new(52, 11).ratio # 4.7272727272727275
