358b8c378aa712b6f087fea67feaca5f5e42bf41
Paul Tobin <tobster111@gmail.com>
2022-11-08 17:36:05 +0000

    Code challenge

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -436,3 +436,6 @@ end
 # Give your answer as a string matching "odd" or "even".
 
 # If the input array is empty consider it as: [0] (array with a zero).
+def odd_or_even(array)
+  array.sum % 2 == 0 ? "even" : "odd"
+end
