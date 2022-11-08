c8a7e999c189f03ebc92dbc20090af54e5ee84f1
Paul Tobin <tobster111@gmail.com>
2022-11-08 17:35:14 +0000

    Stripe

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -428,4 +428,11 @@ def get_real_floor(n)
   return n if n <= 0 
   return n-1 if n>=1 && n<=12
   return n-2 if n>=14
-end
+end 
+
+# Odd or Even? 7kyu
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
---
+++ learn-enough-ruby/stripe/server.rb
@@ -0,0 +1,26 @@
+require 'stripe'
+require 'sinatra'
+
+# This is your test secret API key.
+Stripe.api_key = ENV['STRIPE_API_KEY']
+
+set :static, true
+set :port, 4242
+
+YOUR_DOMAIN = 'http://localhost:4242'
+
+post '/create-checkout-session' do
+  content_type 'application/json'
+
+  session = Stripe::Checkout::Session.create({
+    line_items: [{
+      # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+      price: 'price_1M1dzYIlNg1CJV3j4pkaZkh3',
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: YOUR_DOMAIN + '/success.html',
+    cancel_url: YOUR_DOMAIN + '/cancel.html',
+  })
+  redirect session.url, 303
+end
