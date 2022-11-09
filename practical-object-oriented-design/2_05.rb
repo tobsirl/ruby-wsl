540f6225562475cddc3e976e9c19bfc6963ce205
Paul Tobin <tobster111@gmail.com>
2022-11-09 18:27:06 +0000

    OOP ObscurringReferences

--- practical-object-oriented-design/2_05.rb
+++ practical-object-oriented-design/2_05.rb
@@ -98,3 +98,17 @@ end
 
 puts Blinkered.new.cog(Gear.new(54,11))
 # => private method `cog' called for #<Gear:0x00007fa4ef926120 @chainring=54, @cog=11>
+
+class ObscuringReferences
+  attr_reader :data
+  def initialize(data)
+    @data = data
+  end
+
+  def diameters
+    # 0 is rim, 1 is tire
+    data.collect {|cell|
+      cell[0] + (cell[1] * 2)}
+  end
+  # ... many other methods that index into the array
+end
