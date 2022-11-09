8773d30883a563cc1705cd73a1e7d9c13be61b74
Paul Tobin <tobster111@gmail.com>
2022-11-09 18:33:05 +0000

    OOP RevealingReferences

--- practical-object-oriented-design/2_05.rb
+++ practical-object-oriented-design/2_05.rb
@@ -115,3 +115,21 @@ end
 
 # rim and tire sizes (now in milimeters!) in a 2D array
 @data = [[622, 20], [622, 23], [559, 30], [559, 40]]
+
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect {|wheel|
+      wheel.rim + (wheel.tire * 2)}
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell|
+      Wheel.new(cell[0], cell[1])}
+  end
+end
