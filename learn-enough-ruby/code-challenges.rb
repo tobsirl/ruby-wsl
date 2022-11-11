bff6487657f7e3d50e9aecba588ad7a080c7d0ad
Paul Tobin <tobster111@gmail.com>
2022-11-11 18:42:59 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -481,3 +481,8 @@ end
 def find_it(seq)
   seq.find{|c| seq.count(c).odd? }
 end
+
+# Grasshopper - Terminal game move function 8kyu
+# In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+# Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
