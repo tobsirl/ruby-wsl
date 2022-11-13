1ead7de7b4c41afa82d795e4bf48e0c82fdcd12e
Paul Tobin <tobster111@gmail.com>
2022-11-13 09:27:06 +0000

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -499,3 +499,6 @@ end
 
 # Reversing Words in a String 8kyu
 # You need to write a function that reverses the words in a given string. A word can also fit an empty string.
+def reverse(string)
+  string.split.reverse.join(' ')
+end
