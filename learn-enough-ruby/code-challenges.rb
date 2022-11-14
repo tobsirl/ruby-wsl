0e8b256bf68213025ff47417c5b98d7623367399
Paul Tobin <tobster111@gmail.com>
2022-11-14 11:20:00 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -502,3 +502,8 @@ end
 def reverse(string)
   string.split.reverse.join(' ')
 end
+
+# Find the stray number 7kyu
+# You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+# Complete the method which accepts such an array, and returns that single different number.
