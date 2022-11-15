08c006e4b5286accd978465be81c37bb022e128f
Paul Tobin <tobster111@gmail.com>
2022-11-15 10:56:25 +0000

    Classes playground print_name method

--- learn-enough-ruby/OOP_Projects/classes.rb
+++ learn-enough-ruby/OOP_Projects/classes.rb
@@ -1,10 +1,19 @@
 class Person
-  attr_reader :first_name, :last_name, :age
+  attr_accessor :first_name, :last_name, :age
   def initialize(first_name, last_name, age)
     @first_name = first_name
     @last_name = last_name
     @age = age
   end
+
+  def print_name
+    puts "Name: #{@first_name} #{@last_name}"
+  end
+  
 end
 
 puts paul = Person.new("Paul", "Smith", 25)
+
+paul.last_name = "Tobin"
+
+puts paul.print_name
