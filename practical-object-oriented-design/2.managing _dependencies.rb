0e3ac1259a71cd13bd12ff84c0ffaf06d9f9fd50
Paul Tobin <tobster111@gmail.com>
2022-11-15 10:42:31 +0000

    OOP class Wheel is created in Gear class

--- practical-object-oriented-design/2.managing _dependencies.rb
+++ practical-object-oriented-design/2.managing _dependencies.rb
@@ -69,3 +69,18 @@ class Gear
 end
 
 puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches # 137.0909090909091
+
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog       = cog
+    @wheel     = Wheel.new(rim, tire)
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end 
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches # 137.0909090909091
