fd799eda9300e900bba9ee0596abdc99f0b8481e
Paul Tobin <tobster111@gmail.com>
2022-11-17 14:10:40 +0000

    Code challenge if else

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -550,5 +550,11 @@ end
 # Determine offspring sex based on genes XX and XY chromosomes 8kyu
 # Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
 # If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
-
+def chromosome_check(sperm)
+  if sperm.include?('Y')
+    "Congratulations! You're going to have a son."
+  else
+    "Congratulations! You're going to have a daughter."
+  end
+end
 
