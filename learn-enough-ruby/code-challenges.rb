1fb97ff0875be30eb10a12dca15673291e8c224c
Paul Tobin <tobster111@gmail.com>
2022-11-17 14:10:23 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -546,3 +546,9 @@ end
 def solution(string)
   string.chars.map { |s| s == s.downcase ? s : " #{s}" }.join
 end
+
+# Determine offspring sex based on genes XX and XY chromosomes 8kyu
+# Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+# If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+
