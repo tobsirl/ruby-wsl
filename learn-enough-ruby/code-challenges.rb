4c04ebc544a228e1cff2f8bf10bc43a96e4d2540
Paul Tobin <tobster111@gmail.com>
2022-11-18 14:25:36 +0000

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -571,3 +571,6 @@ end
 # your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. 
 # Your xor function should have the behaviour described above, returning true if exactly one of the 
 # two expressions evaluate to true, false otherwise.
+def xor(a,b)
+  a == b ? false : true
+end
