45c8d4f8d53f7efb1b30143199b6f432770925c1
Paul Tobin <tobster111@gmail.com>
2022-11-18 14:24:18 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -565,3 +565,9 @@ end
 def chromosome_check(sperm)
   return sperm == "XY" ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter.";
 end
+
+# Exclusive "or" (xor) Logical Operator 8kyu
+# Since we cannot define keywords in Javascript (well, at least I don't know how to do it), 
+# your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. 
+# Your xor function should have the behaviour described above, returning true if exactly one of the 
+# two expressions evaluate to true, false otherwise.
