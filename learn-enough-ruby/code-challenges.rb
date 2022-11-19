a3d9243d91ce97150facee9058c2d0e97494f46e
Paul Tobin <tobster111@gmail.com>
2022-11-19 09:17:58 +0000

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -588,3 +588,6 @@ end
 # of the chars) and then return the first value.
 
 # The returned value must be a string, and have "***" between each of its letters.
+def two_sort(s)
+  s.sort.first.chars.join("***")
+end
