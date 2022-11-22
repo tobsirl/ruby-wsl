11851a68cdbe0b1e984edafb6a2273ecbf376bf3
Paul Tobin <tobster111@gmail.com>
2022-11-22 14:22:42 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -627,3 +627,7 @@ end
 def multiple_of_index(array)
   array.select.with_index { |x,i| x % i == 0 unless i.zero?}
 end
+
+# Find the Remainder 8kyu
+# Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+# Division by zero should return an empty value.
