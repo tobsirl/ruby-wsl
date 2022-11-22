a5d4e005d93f9c161188a84a435edc4d665a0c52
Paul Tobin <tobster111@gmail.com>
2022-11-22 18:31:13 +0000

    update

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -631,3 +631,14 @@ end
 # Find the Remainder 8kyu
 # Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
 # Division by zero should return an empty value.
+def remainder(a, b)
+  if a == 0 || b == 0
+    return nil
+  end
+  
+  if a > b
+    a % b
+  else
+    b % a
+  end
+end
