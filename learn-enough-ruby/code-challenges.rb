84fd78b2713f889ea09607baab76de1cdabc888f
Paul Tobin <tobster111@gmail.com>
2022-11-25 16:16:23 +0000

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -739,3 +739,10 @@ end
 # For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
 # The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; 
 # target will always be the sum of two different items from that array).
+def two_sum(numbers, target)
+  (0...numbers.length).each do |i|
+    (i+1...numbers.length).each do |j|
+      return [i, j] if numbers[i] + numbers[j] == target
+    end
+  end
+end
