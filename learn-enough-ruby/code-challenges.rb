719cd456a3e39051bbfb241a0196826c654b8865
Paul Tobin <tobster111@gmail.com>
2022-11-26 13:27:12 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -746,3 +746,9 @@ def two_sum(numbers, target)
     end
   end
 end
+
+# Duplicate Encoder 6kyu
+# The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
+# if that character appears only once in the original string, or ")" 
+# if that character appears more than once in the original string. 
+# Ignore capitalization when determining if a character is a duplicate.
