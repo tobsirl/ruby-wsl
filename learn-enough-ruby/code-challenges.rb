6c6dcceee7d1852a998ffe995b0107810cbc1d7d
Paul Tobin <tobster111@gmail.com>
2022-11-27 10:08:33 +0000

    Code challenge other solutions

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -771,3 +771,23 @@ def longest_consec(strarr, k)
   
   return strarr.each_cons(k).map(&:join).max_by(&:size)
 end
+
+def longest_consec(strarr, k)
+  return "" unless k.between?(1, strarr.length)
+  strarr.each_cons(k).map(&:join).max_by(&:length) || ""
+end
+
+def longest_consec(strarr, k)
+  k > strarr.size || k <= 0 ? '' : strarr.each_cons(k).map(&:join).max_by(&:size)
+end
+
+def longest_consec(strarr, k)
+  return "" if strarr.length == 0 || k > strarr.length || k <= 0
+  
+    longest_string = ""
+    strarr.each_index do |i|
+      str = strarr[i...i+k].join
+      longest_string = str if str.length > longest_string.length
+    end
+    longest_string
+end
