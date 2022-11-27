88b6414b45b0d05fa10b905958718989b65f7ece
Paul Tobin <tobster111@gmail.com>
2022-11-27 10:06:28 +0000

    Code challenge set up

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -761,3 +761,7 @@ def duplicate_encode(word)
   
   arr_of_letters.collect! {|x| duplicates[x] > 1 ? ")" : "("}.join
 end
+
+# Consecutive Strings 6kyu
+# You are given an array(list) strarr of strings and an integer k. 
+# Your task is to return the first longest string consisting of k consecutive strings taken in the array.
