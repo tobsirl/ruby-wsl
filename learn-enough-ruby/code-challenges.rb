f98ef41e8c03ae099a5d612a342c40bbcdfe3113
Paul Tobin <tobster111@gmail.com>
2022-11-28 18:15:13 +0000

    Code challenge solution

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -796,3 +796,8 @@ end
 # Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase 
 # in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase 
 # letter becomes lowercase. 
+class String
+  def to_alternating_case
+    swapcase
+  end
+end
