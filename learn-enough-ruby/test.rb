1f21be8f68dea603fcc2f129da7a62faa6679254
Paul Tobin <tobster111@gmail.com>
2022-12-01 19:37:05 +0000

    Creating a hash of alphabet

--- learn-enough-ruby/test.rb
+++ learn-enough-ruby/test.rb
@@ -1,3 +1,10 @@
-puts "10 20".split(' ').include?("15") ? "US" : "EU"
-puts "10 20".split(' ')
+"10 20".split(' ').include?("15") ? "US" : "EU"
+"10 20".split(' ')
 
+empty_hash = {}
+
+('a'..'z').zip((1..26).to_a).each do |i, c|
+  empty_hash[i] = c
+end
+
+puts empty_hash
