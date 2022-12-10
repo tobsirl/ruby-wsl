bc43be4a2c0617c45f8e163ee2f87068ff306f16
Paul Tobin <tobster111@gmail.com>
2022-12-10 09:39:56 +0000

    Code challenge

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -856,3 +856,10 @@ def century(year)
 def century(year)
   ( year / 100.0 ).ceil
 end
+
+# Regex validate PIN code 7kyu
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+# If the function is passed a valid PIN string, return true, else return false.
+def validate_pin(pin)
+  pin.match(/\A\d{4}\z|\A\d{6}\z/) ? true : false
+end
