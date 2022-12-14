7c62b02d57e9cd0ad0efe5476df5440be15a07b9
Paul Tobin <tobster111@gmail.com>
2022-12-14 10:37:30 +0000

    Code old

--- learn-enough-ruby/index.js
+++ learn-enough-ruby/index.js
@@ -68,3 +68,10 @@ function getDivisorsCnt(n) {
   }
   return count;
 }
+
+// Regular Ball Super Ball 8kyu
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+var Ball = function (ballType) {
+  this.ballType = ballType || 'regular';
+}
