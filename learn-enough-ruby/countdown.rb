b864c34b75ad308a37b456657cd1bd01087c0e13
Paul Tobin <tobster111@gmail.com>
2022-12-14 10:35:21 +0000

    countdown

---
+++ learn-enough-ruby/countdown.rb
@@ -0,0 +1,7 @@
+# 14 day countdown that will display the same message each day
+# and then display a message on the last day
+# start with 14
+# subtract 1 each day
+# display the number of days left
+# display a message on the last day
+
