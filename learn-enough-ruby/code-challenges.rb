3f61bfd9b3ed429ec9832a7f47e31541aadd7e23
Paul Tobin <tobster111@gmail.com>
2022-12-14 10:44:28 +0000

    Code class

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -881,3 +881,14 @@ end
 def divisors(n)
   (1..n).count { |x| n % x == 0 }
 end
+
+# Regular Ball Super Ball 8kyu
+# Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+# If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+class Ball
+  attr_accessor :ball_type
+  
+  def initialize(ball_type = "regular")
+    @ball_type = ball_type
+  end
+end
