e8c03f9fad34a9e9d0a4b25ca711c82a40b79020
Paul Tobin <tobster111@gmail.com>
2022-12-15 09:14:30 +0000

    Code js

--- learn-enough-ruby/index.js
+++ learn-enough-ruby/index.js
@@ -81,3 +81,10 @@ class Ball {
     this.ballType = ballType;
   }
 }
+
+// Unique In Order 6kyu
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements
+//  with the same value next to each other and preserving the original order of elements.
+var uniqueInOrder = function (iterable) {
+  return [...iterable].filter((a, i) => a !== iterable[i - 1]);
+}
