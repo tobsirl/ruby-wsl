13d3cc0a48a4556ab56307bf9551dba9c00629a0
Paul Tobin <tobster111@gmail.com>
2022-12-15 09:11:01 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -892,3 +892,12 @@ class Ball
     @ball_type = ball_type
   end
 end
+
+# Unique In Order 6kyu
+# Implement the function unique_in_order which takes as argument a sequence and 
+# returns a list of items without any elements with the same value next to each other and 
+# preserving the original order of elements.
+def unique_in_order(iterable)
+  iterable = iterable.split("") if iterable.is_a? String
+  iterable.chunk(&:itself).map(&:first)
+end
