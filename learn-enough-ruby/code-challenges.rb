9f21f53846493d200d754246a27e2d31e201b273
Paul Tobin <tobster111@gmail.com>
2022-12-19 09:10:23 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -934,3 +934,16 @@ def remove_smallest(numbers)
   numbers.delete_at(numbers.index(numbers.min)) unless numbers.empty?
   numbers
 end
+
+# How many stairs will Suzuki climb in 20 years? 8kyu
+# Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. 
+# Some days he climbs more stairs than others depending on the number of students he must train in the morning. 
+# He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
+# The sum of all the stairs logged in a year will be used for estimating the number he might climb in 20.
+# 20_year_estimate = one_year_total * 20
+# You will receive the following data structure representing the stairs Suzuki logged in a year.
+# stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
+# Make the function stairs_in_20(stairs) return the 20 year estimate of the stairs climbed using the formula above.
+def stairs_in_20(stairs)
+  stairs.flatten.reduce(:+) * 20
+end
