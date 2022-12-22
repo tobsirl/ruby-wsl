885a7de762643ed8d1d182d92714a8aa7af05924
Paul Tobin <tobster111@gmail.com>
2022-12-22 08:46:39 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -962,3 +962,18 @@ end
 def flatten_and_sort(array)
   array.flatten.sort
 end
+
+# Return the day 8kyu
+# Complete the function which returns the weekday according to the input number:
+def what_day(n)
+  case n
+    when 1 then "Sunday"
+    when 2 then "Monday"
+    when 3 then "Tuesday"
+    when 4 then "Wednesday"
+    when 5 then "Thursday"
+    when 6 then "Friday"
+    when 7 then "Saturday"
+    else "Wrong, please enter a number between 1 and 7"
+  end
+end
