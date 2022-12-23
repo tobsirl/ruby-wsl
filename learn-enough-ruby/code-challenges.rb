479efd912190453a1309f2172753df781f257d43
Paul Tobin <tobster111@gmail.com>
2022-12-23 11:34:35 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -977,3 +977,9 @@ def what_day(n)
     else "Wrong, please enter a number between 1 and 7"
   end
 end
+
+# Multiplication table for number 8kyu
+# Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+def multi_table(number)
+  (1..10).map { |i| "#{i} * #{number} = #{i*number}" }.join("\n")
+end
