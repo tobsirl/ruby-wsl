8842709e58a8321aae46944f234a81b4e4a8268e
Paul Tobin <tobster111@gmail.com>
2022-12-28 09:18:22 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1009,3 +1009,31 @@ def say_hello(name, city, state)
   "Hello, #{name.join(" ")}! Welcome to #{city}, #{state}!"
 end
 
+# I love you, a little , a lot, passionately ... not at all 8kyu
+# Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, 
+# saying each of the following phrases each time a petal was torn:
+# 1. "I love you"
+# 2. "a little"
+# 3. "a lot"
+# 4. "passionately"
+# 5. "madly"
+# 6. "not at all"
+# If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+# Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. 
+# The number of petals is always greater than 0.
+def how_much_i_love_you(nb_petals)
+  case nb_petals % 6
+    when 1 then
+      "I love you"
+    when 2 then
+      "a little"
+    when 3 then
+      "a lot"
+    when 4 then
+      "passionately"
+    when 5 then
+      "madly"
+    when 0 then
+      "not at all"
+  end
+end
