a5f5d5d4f9522fe21fa4dd04669e7b3c01a6d63b
Paul Tobin <tobster111@gmail.com>
2022-12-30 09:19:48 +0000

    Code Title Case

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1046,3 +1046,15 @@ end
 def capitalize(s)
   [s.chars.map.with_index { |c, i| i.even? ? c.upcase : c }.join, s.chars.map.with_index { |c, i| i.odd? ? c.upcase : c }.join]
 end
+
+# Title Case 6kyu
+# A string is considered to be in title case if each word in the string is either (a) capitalised 
+# (that is, only the first letter of the word is in upper case) or (b) considered to be an exception 
+# and put entirely into lower case unless it is the first word, which is always capitalised.
+# Write a function that will convert a string into title case, given an optional list of exceptions (minor words). 
+# The list of minor words will be given as a string with each word separated by a space. 
+# Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor 
+# word string is changed.
+def title_case(title, minor_words = '')
+  title.capitalize.split.map { |word| minor_words.downcase.split.include?(word) ? word : word.capitalize }.join(' ')
+end
