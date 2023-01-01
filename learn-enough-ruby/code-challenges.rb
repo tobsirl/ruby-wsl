43f9a2be9166f73185230faefdb78c06aaa9b133
Paul Tobin <tobster111@gmail.com>
2023-01-01 09:45:22 +0000

    Code Sum of numbers from 0 to N

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1069,3 +1069,11 @@ end
 def pipe_fix(numbers)
   (numbers.min..numbers.max).to_a
 end
+
+# Sum of numbers from 0 to N 7kyu
+# We want to generate a function that computes the series starting from 0 and ending until the given number.
+class SequenceSum
+  def self.show_sequence(n)
+    (n < 0) ? "#{n}<0" : (n == 0) ? "0=0" : "#{(0..n).to_a.join("+")} = #{(0..n).to_a.sum}"
+  end
+end
