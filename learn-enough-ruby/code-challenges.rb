ca302f2b69f560d8a50e6259ba478b36cbc4b09f
Paul Tobin <tobster111@gmail.com>
2023-01-06 09:18:49 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1097,3 +1097,14 @@ end
 def unusual_five
   "five!".length
 end
+
+# Row Weights 7kyu
+# Scenario
+# Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes into team 2,
+# the third goes into team 1, and so on.
+# Task
+# Given an array of positive integers (the weights of the people), return a new array/tuple of two integers,
+# where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+def row_weights(array)
+  array.each_slice(2).map { |a, b| [a.to_i, b.to_i] }.transpose.map(&:sum)
+end
