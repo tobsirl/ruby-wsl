3e94dcd85869124cda44b2d717151e7d6b7a4d8f
Paul Tobin <tobster111@gmail.com>
2023-01-08 09:41:00 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1116,3 +1116,11 @@ end
 def is_isogram(string)
   string.downcase.chars.uniq == string.downcase.chars
 end
+
+# Sum of sequence 7kyu
+# Your task is to make function, which returns the sum of a sequence of integers.
+# The sequence is defined by 3 non-negative values: begin, end, step.
+# If begin value is greater than the end, function should returns 0
+def sequence_sum(begin_number, end_number, step)
+  (begin_number..end_number).step(step).sum
+end
