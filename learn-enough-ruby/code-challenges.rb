070d5b75a0159bc2a8c5ecd35e898916f4f75aa7
Paul Tobin <tobster111@gmail.com>
2023-01-09 14:13:13 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1124,3 +1124,12 @@ end
 def sequence_sum(begin_number, end_number, step)
   (begin_number..end_number).step(step).sum
 end
+
+# Find the next perfect square! 7kyu
+# You might know some pretty large perfect squares. But what about the NEXT one?
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+# Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+# If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
+def find_next_square(sq)
+  (Math.sqrt(sq) % 1).zero? ? (Math.sqrt(sq) + 1)**2 : -1
+end
