71f3e0a7a65006562974b1fbfe6de5bdb3afe002
Paul Tobin <tobster111@gmail.com>
2023-01-14 09:56:26 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1162,3 +1162,14 @@ end
 def position(alphabet)
   "Position of alphabet: #{alphabet.ord - 96}"
 end
+
+# The highest profit wins! 7kyu
+# Story
+# Ben has a very simple idea to make some profit: he buys something and sells it again. 
+# Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. 
+# Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+# Task
+# Write a function that returns both the minimum and maximum number of the given list/array.
+def min_max(lst)
+  [lst.min, lst.max]
+end
