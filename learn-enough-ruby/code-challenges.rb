f48e5a7ecf489883eb2ea4460381b5cd4459e951
Paul Tobin <tobster111@gmail.com>
2023-01-24 18:20:54 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1224,3 +1224,12 @@ end
 def accum(s)
   s.chars.map.with_index { |c, i| c.upcase + c.downcase * i }.join('-')
 end
+
+# Count the Digit 7kyu
+# Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+# Square all numbers k (0 <= k <= n) between 0 and n.
+# Count the numbers of digits d used in the writing of all the k**2.
+# Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+def nb_dig(n, d)
+  (0..n).map { |i| i**2 }.join.count(d.to_s)
+end
