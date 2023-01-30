0f26bb82155d15070f5a5314a4f84dcf6664a7e0
Paul Tobin <tobster111@gmail.com>
2023-01-30 17:34:11 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1283,3 +1283,12 @@ end
 def combine_names(first_name, last_name)
   "#{first_name} #{last_name}"
 end
+
+# Sorted? yes? no? how? 7kyu
+# Complete the method which accepts an array of integers, and returns one of the following:
+# "yes, ascending" - if the numbers in the array are sorted in an ascending order
+# "yes, descending" - if the numbers in the array are sorted in a descending order
+# "no" - otherwise
+def is_sorted_and_how(arr)
+  arr == arr.sort ? "yes, ascending" : arr == arr.sort.reverse ? "yes, descending" : "no"
+end
