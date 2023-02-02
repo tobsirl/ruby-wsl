51c9913ffa44b66e441552d3a48a05dac14c9a8e
Paul Tobin <tobster111@gmail.com>
2023-02-02 18:26:39 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1309,3 +1309,14 @@ end
 def sumDigits(number)
   number.abs.to_s.chars.map(&:to_i).sum
 end
+
+# Growth of a Population 7kyu
+# In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+def nb_year(p0, percent, aug, p)
+  years = 0
+  while p0 < p
+    p0 += p0 * (percent / 100.0) + aug
+    years += 1
+  end
+  years
+end
