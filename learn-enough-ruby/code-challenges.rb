029df6875f601090106c827186a055304f4418bc
Paul Tobin <tobster111@gmail.com>
2023-02-03 10:23:25 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1317,3 +1317,19 @@ end
 def array(string)
   string.split(',').slice(1..-2).join(' ') if string.split(',').length > 2
 end
+
+# Speed Control 7kyu
+# In John's car the GPS records every s seconds the distance travelled from an origin (distances are measured in an arbitrary but consistent unit).
+# For example, below is part of a record with s = 15:
+# # time  distance
+# x = [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+# The sections are:
+# 0.0-0.19, 0.19-0.5, 0.5-0.75, 0.75-1.0, 1.0-1.25, 1.25-1.50, 1.5-1.75, 1.75-2.0, 2.0-2.25
+# We can calculate John's average hourly speed on every section and we get:
+# [47.27, 72.24, 72.24, 72.24, 72.24, 72.24, 72.24, 72.24, 72.24]
+# Given s and x the task is to return as an integer the *floor* of the maximum average speed per hour obtained on the sections of x.
+# If x length is less than or equal to 1 return 0 since the car didn't move.
+def gps(s, x)
+  return 0 if x.length <= 1
+  (x.each_cons(2).map { |a, b| (3600 * (b - a) / s).floor }.max)
+end
