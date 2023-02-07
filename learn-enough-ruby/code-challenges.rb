802f1a745a3beefd55f72e7045ca77869ffdbc2d
Paul Tobin <tobster111@gmail.com>
2023-02-07 18:30:03 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1351,3 +1351,10 @@ end
 def printArray(array)
   array.join(',')
 end
+
+# Find the middle element 7kyu
+# As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+# The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+def gimme(input_array)
+  input_array.index(input_array.sort[1])
+end
