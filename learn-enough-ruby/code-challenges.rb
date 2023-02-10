25eed7dcce7dabe8221bdc6a53588a2ff55803fa
Paul Tobin <tobster111@gmail.com>
2023-02-10 15:45:01 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1370,3 +1370,22 @@ end
 def greet(name)
   "Hello #{name.capitalize}!"
 end
+
+# Tip Calculator 8kyu
+# Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+def calculate_tip(amount, rating)
+  case rating.downcase
+  when "terrible"
+    0
+  when "poor"
+    (amount * 0.05).ceil
+  when "good"
+    (amount * 0.1).ceil
+  when "great"
+    (amount * 0.15).ceil
+  when "excellent"
+    (amount * 0.2).ceil
+  else
+    "Rating not recognised"
+  end
+end
