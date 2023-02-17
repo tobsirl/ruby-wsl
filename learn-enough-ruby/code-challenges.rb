0ff814e588e15644b8ad3a965ba40526c90b9523
Paul Tobin <tobster111@gmail.com>
2023-02-17 12:47:57 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1443,3 +1443,10 @@ class Sleigh
     name == "Santa Claus" && password == "Ho Ho Ho!"
   end
 end
+
+# Sum of Cubes 7kyu
+# Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+# Assume that the input n will always be a positive integer.
+def sum_cubes(n)
+  (1..n).map { |x| x**3 }.sum
+end
