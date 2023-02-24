7bc22b67fce4ad946a7d023c045045f4d8737cdf
Paul Tobin <tobster111@gmail.com>
2023-02-24 17:31:39 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1537,3 +1537,48 @@ end
 def shorten_to_date(long_date)
   long_date.split(',')[0]
 end
+
+# Alphabet war - airstrike - letters massacre 7kyu
+# Introduction
+# There is a war and nobody knows - the alphabet war!
+# There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+# Task
+# Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+  
+  # The left side letters and their power:
+  #  w - 4
+  #  p - 3 
+  #  b - 2
+  #  s - 1
+  # The right side letters and their power:
+  #  m - 4
+  #  q - 3 
+  #  d - 2
+  #  z - 1
+  # The other letters don't have power and are only victims.
+  # The *-symbol powers are doubled (look at the examples below).
+  # The letters called airstrike (a, j and y) kill the adjacent letters ( i.e. a kills z, j kills s and y kills t). The killed letters don't 
+  # fight back.
+  # The airstrike always works from left to right.
+  # Example
+  # alphabet_war("s*zz");           //=> Right side wins!
+  # alphabet_war("*zd*qm*wp*bs*"); //=> Let's fight again!
+  # alphabet_war("zzzz*s*");       //=> Right side wins!
+  # alphabet_war("www*www****z");  //=> Left side wins!
+  def alphabet_war(fight)
+    left = { 'w' => 4, 'p' => 3, 'b' => 2, 's' => 1 }
+    right = { 'm' => 4, 'q' => 3, 'd' => 2, 'z' => 1 }
+    fight = fight.gsub(/a|j|y/, '')
+    fight = fight.gsub(/s\*|z\*|t\*/, 's').gsub(/\*s|\*z|\*t/, 's')
+    fight = fight.gsub(/w\*|p\*|b\*|s\*/, 'w').gsub(/\*w|\*p|\*b|\*s/, 'w')
+    fight = fight.gsub(/m\*|q\*|d\*|z\*/, 'm').gsub(/\*m|\*q|\*d|\*z/, 'm')
+    left_power = fight.chars.map { |x| left[x] }.compact.sum
+    right_power = fight.chars.map { |x| right[x] }.compact.sum
+    if left_power > right_power
+      "Left side wins!"
+    elsif left_power < right_power
+      "Right side wins!"
+    else
+      "Let's fight again!"
+    end
+  end
