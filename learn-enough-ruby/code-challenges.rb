2cdd4a6bb1a23748ef4ea496056544b459812c6f
Paul Tobin <tobster111@gmail.com>
2023-02-25 08:55:43 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1582,3 +1582,13 @@ end
       "Let's fight again!"
     end
   end
+
+  # Array.diff 6kyu
+  # Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+  # It should remove all values from list a, which are present in list b.
+  # array_diff([1,2],[1]) == [2]
+  # If a value is present in b, all of its occurrences must be removed from the other:
+  # array_diff([1,2,2,2,3],[2]) == [1,3]
+  def array_diff(a, b)
+    a - b
+  end
