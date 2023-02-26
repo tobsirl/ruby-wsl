68d83c70dcb8bf491b51fb8f24a0d9cc238aeaeb
Paul Tobin <tobster111@gmail.com>
2023-02-26 08:52:16 +0000

    Code fix formatting

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1545,50 +1545,58 @@ end
 # Task
 # Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
   
-  # The left side letters and their power:
-  #  w - 4
-  #  p - 3 
-  #  b - 2
-  #  s - 1
-  # The right side letters and their power:
-  #  m - 4
-  #  q - 3 
-  #  d - 2
-  #  z - 1
-  # The other letters don't have power and are only victims.
-  # The *-symbol powers are doubled (look at the examples below).
-  # The letters called airstrike (a, j and y) kill the adjacent letters ( i.e. a kills z, j kills s and y kills t). The killed letters don't 
-  # fight back.
-  # The airstrike always works from left to right.
-  # Example
-  # alphabet_war("s*zz");           //=> Right side wins!
-  # alphabet_war("*zd*qm*wp*bs*"); //=> Let's fight again!
-  # alphabet_war("zzzz*s*");       //=> Right side wins!
-  # alphabet_war("www*www****z");  //=> Left side wins!
-  def alphabet_war(fight)
-    left = { 'w' => 4, 'p' => 3, 'b' => 2, 's' => 1 }
-    right = { 'm' => 4, 'q' => 3, 'd' => 2, 'z' => 1 }
-    fight = fight.gsub(/a|j|y/, '')
-    fight = fight.gsub(/s\*|z\*|t\*/, 's').gsub(/\*s|\*z|\*t/, 's')
-    fight = fight.gsub(/w\*|p\*|b\*|s\*/, 'w').gsub(/\*w|\*p|\*b|\*s/, 'w')
-    fight = fight.gsub(/m\*|q\*|d\*|z\*/, 'm').gsub(/\*m|\*q|\*d|\*z/, 'm')
-    left_power = fight.chars.map { |x| left[x] }.compact.sum
-    right_power = fight.chars.map { |x| right[x] }.compact.sum
-    if left_power > right_power
-      "Left side wins!"
-    elsif left_power < right_power
-      "Right side wins!"
-    else
-      "Let's fight again!"
-    end
+# The left side letters and their power:
+#  w - 4
+#  p - 3 
+#  b - 2
+#  s - 1
+# The right side letters and their power:
+#  m - 4
+#  q - 3 
+#  d - 2
+#  z - 1
+# The other letters don't have power and are only victims.
+# The *-symbol powers are doubled (look at the examples below).
+# The letters called airstrike (a, j and y) kill the adjacent letters ( i.e. a kills z, j kills s and y kills t). The killed letters don't 
+# fight back.
+# The airstrike always works from left to right.
+# Example
+# alphabet_war("s*zz");           //=> Right side wins!
+# alphabet_war("*zd*qm*wp*bs*"); //=> Let's fight again!
+# alphabet_war("zzzz*s*");       //=> Right side wins!
+# alphabet_war("www*www****z");  //=> Left side wins!
+def alphabet_war(fight)
+  left = { 'w' => 4, 'p' => 3, 'b' => 2, 's' => 1 }
+  right = { 'm' => 4, 'q' => 3, 'd' => 2, 'z' => 1 }
+  fight = fight.gsub(/a|j|y/, '')
+  fight = fight.gsub(/s\*|z\*|t\*/, 's').gsub(/\*s|\*z|\*t/, 's')
+  fight = fight.gsub(/w\*|p\*|b\*|s\*/, 'w').gsub(/\*w|\*p|\*b|\*s/, 'w')
+  fight = fight.gsub(/m\*|q\*|d\*|z\*/, 'm').gsub(/\*m|\*q|\*d|\*z/, 'm')
+  left_power = fight.chars.map { |x| left[x] }.compact.sum
+  right_power = fight.chars.map { |x| right[x] }.compact.sum
+  if left_power > right_power
+    "Left side wins!"
+  elsif left_power < right_power
+    "Right side wins!"
+  else
+    "Let's fight again!"
   end
+end
 
-  # Array.diff 6kyu
-  # Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-  # It should remove all values from list a, which are present in list b.
-  # array_diff([1,2],[1]) == [2]
-  # If a value is present in b, all of its occurrences must be removed from the other:
-  # array_diff([1,2,2,2,3],[2]) == [1,3]
-  def array_diff(a, b)
-    a - b
-  end
+# Array.diff 6kyu
+# Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+# It should remove all values from list a, which are present in list b.
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+def array_diff(a, b)
+  a - b
+end
+
+# Check same case 8kyu
+# Write a function that will check if two given characters are the same case.
+# If either of the characters is not a letter, return -1
+# If both characters are the same case, return 1
+# If both characters are letters, but not the same case, return 0
+# Examples
+
