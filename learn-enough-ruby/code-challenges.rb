396cf8300ffe83ba54afc8e57537b7af58387aee
Paul Tobin <tobster111@gmail.com>
2023-02-28 17:32:05 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1623,3 +1623,17 @@ def count_by(x, n)
   (1..n).map { |i| i * x }
 end
 
+# Contamination #1 -String 8kyu
+# An AI has infected a text with a character!!
+# This text is now fully mutated to this character.
+# If the text or the character are empty, return an empty string.
+# There will never be a case when both are empty as nothing is going on!!
+# Note: The character is a string of length 1 or an empty string.
+# Example
+# text before = "abc"
+# character   = "z"
+# text after  = "zzz"
+def contamination(text, char)
+  char * text.length
+end
+
