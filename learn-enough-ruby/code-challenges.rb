d6503fef54a2cf925b28f774f690a7b6649c755e
Paul Tobin <tobster111@gmail.com>
2023-03-02 18:20:14 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1642,3 +1642,13 @@ end
 def get_size(w, h, d)
   [2 * (w * h + w * d + h * d), w * h * d]
 end
+
+# Powers of 2 8kyu
+# Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+# Examples
+# n = 0  ==> [1]        # [2^0]
+# n = 1  ==> [1, 2]     # [2^0, 2^1]
+# n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+def powers_of_two(n)
+  (0..n).map { |i| 2 ** i }
+end
