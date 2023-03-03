832e9854dd1881673958b78bcae65b857e0f3750
Paul Tobin <tobster111@gmail.com>
2023-03-03 09:54:28 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1652,3 +1652,15 @@ end
 def powers_of_two(n)
   (0..n).map { |i| 2 ** i }
 end
+
+# Find out whether the shape is a cube 8kyu
+# To find the volume (centimeters cubed) of a cuboid you use the formula:
+# volume = Length * Width * Height
+# But someone forgot to use proper record keeping, so we only have the volume, and the length of a single side!
+# It's up to you to find out whether the cuboid has equal sides (= is a cube).
+# Return true if the cuboid could have equal sides, return false otherwise.
+# Return false for invalid numbers too (e.g volume or side is less than or equal to 0).
+# Note: the sides must be integers
+def cube_checker(volume, side)
+  side > 0 && volume > 0 && volume % side == 0
+end
