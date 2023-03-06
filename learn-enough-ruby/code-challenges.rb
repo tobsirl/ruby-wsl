e838f31f46018acdd10a3366e021efce73a60886
Paul Tobin <tobster111@gmail.com>
2023-03-06 18:07:41 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1694,3 +1694,12 @@ def leo(oscar)
     "Leo got one already!"
   end
 end
+
+# Who ate the cookie? 8kyu
+# For this problem you must create a program that says who ate the last cookie. 
+# If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. 
+# If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+# Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach!"
+def cookie(x)
+  "Who ate the last cookie? It was #{x.class == String ? 'Zach' : x.class == Float || x.class == Fixnum ? 'Monica' : 'the dog'}!"
+end
