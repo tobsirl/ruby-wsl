d59f83bf4fba7c22aa371469c714c1547222af2c
Paul Tobin <tobster111@gmail.com>
2023-03-08 17:51:42 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1725,3 +1725,10 @@ end
 def highest_rank(arr)
   arr.group_by { |i| i }.max_by { |k, v| v.size }[0]
 end
+
+# Find the vowels 7kyu
+# We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+# So given a string "super", we should return a list of [2, 4].
+def vowel_indices(word)
+  word.chars.map.with_index { |i, index| index + 1 if 'aeiouy'.include?(i.downcase) }.compact
+end
