6a5418ca3d86e50961286c0cd380c580490d6e1b
Paul Tobin <tobster111@gmail.com>
2023-03-12 09:29:03 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1756,3 +1756,14 @@ end
 def cube_odd(arr)
   arr.all? { |i| i.is_a? Numeric } ? arr.select { |i| i.odd? }.map { |i| i ** 3 }.reduce(:+) : nil
 end
+
+# How good are you really? 8kyu
+# There was a test in your class and you passed it. Congratulations!
+# But you're an ambitious person. You want to know if you're better than the average student in your class.
+# You receive an array with your peers' test scores. Now calculate the average and compare your score!
+# Return True if you're better, else False!
+# Note:
+# Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+def better_than_average(arr, points)
+  (arr.reduce(:+) + points) / (arr.size + 1) < points
+end
