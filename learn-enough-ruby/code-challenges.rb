d874024f6b5d1e60def9a211c439d302eb4fa3ab
Paul Tobin <tobster111@gmail.com>
2023-03-14 15:47:22 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1775,3 +1775,16 @@ end
 def ordered_count(str)
   str.chars.uniq.map { |i| [i, str.count(i)] }
 end
+
+# Template Strings
+# Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+# Task
+# Your task is to return the correct string using the Template String Feature.
+# Input
+# Two Strings, no validation is needed.
+# Output
+# You must output a string containing the two strings with the word ```' are '```
+# Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+def TempleStrings(obj, feature)
+  "#{obj} are #{feature}"
+end
