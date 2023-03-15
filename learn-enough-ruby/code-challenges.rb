7f2515c48705916c50673f8fb6ba28599abea978
Paul Tobin <tobster111@gmail.com>
2023-03-15 18:18:27 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1788,3 +1788,12 @@ end
 def TempleStrings(obj, feature)
   "#{obj} are #{feature}"
 end
+
+# Alphabet symmetry 7kyu
+# Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+# Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+# solve(["abode","ABc","xyzD"]) = [4, 3, 1]. See test cases for more examples.
+# Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+def solve(arr)
+  arr.map { |i| i.chars.select.with_index { |i, index| i.downcase == ('a'..'z').to_a[index] }.size }
+end
