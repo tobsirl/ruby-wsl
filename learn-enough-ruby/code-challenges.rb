2553566eb9c8ffb82ab44e28602efadc957d6336
Paul Tobin <tobster111@gmail.com>
2023-03-17 09:54:42 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1803,3 +1803,12 @@ end
 def angle(n)
   (n - 2) * 180
 end
+
+# max diff - easy
+# You must implement a function maxDiff that return the difference between the biggest and the smallest value in a list(lst) received as parameter.
+# The list(lst) contains integers, that means it may contain some negative numbers.
+# If the list is empty or contains a single element, return 0.
+# The list(lst) is not sorted.
+def max_diff(lst)
+  lst.empty? ? 0 : lst.max - lst.min
+end
