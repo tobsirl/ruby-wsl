c374911096c3e82cced6f6465cdda51644ff342d
Paul Tobin <tobster111@gmail.com>
2023-03-19 10:38:53 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1819,3 +1819,13 @@ end
 def find_longest(arr)
   arr.max_by { |i| i.to_s.size }
 end
+
+# Sum of differences in array 8kyu
+# Your task is to sum the differences between consecutive pairs in the array in descending order.
+# For example: sumOfDifferences([2, 1, 10]) Returns 9
+# Descending order: [10, 2, 1]
+# Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+# If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+def sum_of_differences(arr)
+  arr.sort.reverse.each_cons(2).map { |i, j| i - j }.reduce(:+) || 0
+end
