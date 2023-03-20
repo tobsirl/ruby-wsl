7dc0849dafe4feb644d878a9f39b5c46c68fab33
Paul Tobin <tobster111@gmail.com>
2023-03-20 17:49:53 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1829,3 +1829,18 @@ end
 def sum_of_differences(arr)
   arr.sort.reverse.each_cons(2).map { |i, j| i - j }.reduce(:+) || 0
 end
+
+# Predict your age! 7kyu
+# My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+# In honor of my grandfather's memory we will write a function using his formula!
+# Take a list of ages when each of your great-grandparent died.
+# Multiply each number by itself.
+# Add them all together.
+# Take the square root of the result.
+# Divide by two.
+# Example
+# predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+# Note: the result should be rounded down to the nearest integer.
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8)
+  (Math.sqrt(age_1 ** 2 + age_2 ** 2 + age_3 ** 2 + age_4 ** 2 + age_5 ** 2 + age_6 ** 2 + age_7 ** 2 + age_8 ** 2) / 2).floor
+end
