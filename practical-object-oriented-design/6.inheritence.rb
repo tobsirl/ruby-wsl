c056f55cd149ae1e2d1bbbd0774216fcf641a57e
Paul Tobin <tobster111@gmail.com>
2023-03-21 16:05:29 +0000

    Inheritence

--- practical-object-oriented-design/6.inheritence.rb
+++ practical-object-oriented-design/6.inheritence.rb
@@ -2,7 +2,16 @@ class Bicycle
   attr_reader :size, :tape_color
 
   def initialize(**opts)
-    @size       = args[:size]
-    @tape_color = args[:tape_color]
+    @size       = opts[:size]
+    @tape_color = opts[:tape_color]
+  end
+
+  # Every bike has the same defaults for tire and chain size
+  def spares
+    { chain: '10-speed', tire_size: '23', tape_color: tape_color }
   end
 end
+
+bike = Bicycle.new(size: 'M', tape_color: 'red')
+puts bike.size
+puts bike.spares
