748c06fe744fd3b86139ed7ba3ad76c9ae011594
Paul Tobin <tobster111@gmail.com>
2023-03-22 19:03:10 +0000

    class

--- practical-object-oriented-design/6.inheritence.rb
+++ practical-object-oriented-design/6.inheritence.rb
@@ -1,17 +1,30 @@
+# class Bicycle
+#   attr_reader :size, :tape_color
+
+#   def initialize(**opts)
+#     @size       = opts[:size]
+#     @tape_color = opts[:tape_color]
+#   end
+
+#   # Every bike has the same defaults for tire and chain size
+#   def spares
+#     { chain: '10-speed', tire_size: '23', tape_color: tape_color }
+#   end
+# end
+
+# bike = Bicycle.new(size: 'M', tape_color: 'red')
+# puts bike.size
+# puts bike.spares
+
+# Embedding Multiple Types
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
 
   def initialize(**opts)
-    @size       = opts[:size]
-    @tape_color = opts[:tape_color]
-  end
-
-  # Every bike has the same defaults for tire and chain size
-  def spares
-    { chain: '10-speed', tire_size: '23', tape_color: tape_color }
+    @style       = opts[:style]
+    @size        = opts[:size]
+    @tape_color  = opts[:tape_color]
+    @front_shock = opts[:front_shock]
+    @rear_shock  = opts[:rear_shock]
   end
 end
-
-bike = Bicycle.new(size: 'M', tape_color: 'red')
-puts bike.size
-puts bike.spares
