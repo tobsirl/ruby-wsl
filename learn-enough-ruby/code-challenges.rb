76cbbdda4b2a7bb57cd27c8bea6e01993bdd29e3
Paul Tobin <tobster111@gmail.com>
2023-03-22 15:55:02 +0000

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -1854,3 +1854,13 @@ end
 def rot13(string)
   string.tr('A-Za-z', 'N-ZA-Mn-za-m')
 end
+
+# Convert an array of strings to array of numbers 7kyu
+# Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+# You need to cast the whole array to the correct type.
+# Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+# ie:["1", "2", "3"] to [1, 2, 3]
+# Note that you can receive floats as well.
+def to_float_array(arr)
+  arr.map(&:to_f)
+end
