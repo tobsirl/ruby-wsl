4ad0a527a5aafedce75102f3ddbb7c3076307bab
Paul Tobin <tobster111@gmail.com>
2023-03-26 10:01:01 +0100

    Code

--- learn-enough-ruby/code.rb
+++ learn-enough-ruby/code.rb
@@ -1,13 +1,12 @@
 def min_sum(arr)
   result = 0
   arr.sort.each do |i, j|
-   i = arr.min
-   j = arr.max
-    result += i * j
-    arr[i - 1]
-    arr[j - 1] 
-    puts i 
-    puts j
+    puts arr
+    if arr.size == 0
+      break
+    else 
+      result += arr.shift * arr.pop
+    end
   end
   result
 end
