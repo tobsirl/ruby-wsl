bf73c65e07797f496e8c173ad056ce8fba53b334
Paul Tobin <tobster111@gmail.com>
2023-04-02 09:17:15 +0100

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -2046,3 +2046,12 @@ end
 def nth_smallest(arr, pos)
   arr.sort[pos - 1]
 end
+
+# Enumerable Magic #1 - True for All?
+# Create a method all which takes two params:
+# a sequence
+# a function (function pointer in C)
+# and returns true if the function in the params returns true for every item in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+def all? array, &block
+  array.all?(&block)
+end
