7bbb705afc46108763148a4e1a3d4398f11e57c1
Paul Tobin <tobster111@gmail.com>
2023-04-03 17:30:34 +0100

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -2055,3 +2055,10 @@ end
 def all? array, &block
   array.all?(&block)
 end
+
+# Area of a Square 8kyu
+# Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+# Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+def square_area(a)
+  ((2 * a / Math::PI)**2).round(2)
+end
