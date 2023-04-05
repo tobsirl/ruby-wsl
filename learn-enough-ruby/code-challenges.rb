0f28ee0fed9fa26623a7c900c788c46d0b397c06
Paul Tobin <tobster111@gmail.com>
2023-04-05 14:32:25 +0100

    Code

--- learn-enough-ruby/code-challenges.rb
+++ learn-enough-ruby/code-challenges.rb
@@ -2080,4 +2080,12 @@ def partlist(arr)
   result
 end
 
+# Switcheroo 7kyu
+# Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+# Example:
+# 'acb' --> 'bca'
+# 'aabacbaa' --> 'bbabcabb'
+def switcheroo(x)
+  x.tr('ab', 'ba')
+end
 
