ef8d1bf22a2eb2fc9d170807b95bd97ad0d46f94
Paul Tobin <tobster111@gmail.com>
2023-04-07 09:38:57 +0100

    Code

---
+++ codewars/8kyu.rb
@@ -0,0 +1,8 @@
+# ASCII Total 8kyu
+# You'll be given a string, and have to return the sum of all characters as an int. 
+# The function should be able to handle all printable ASCII characters.
+# Examples
+# uniTotal("a") == 97 uniTotal("aaa") == 291
+def uni_total(str)
+  str.chars.map(&:ord).sum
+end
