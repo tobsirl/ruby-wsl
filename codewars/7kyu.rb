d078c501aaf101ce04d321994431e1cc8c62976c
Paul Tobin <tobster111@gmail.com>
2023-04-08 09:14:33 +0100

    Code

---
+++ codewars/7kyu.rb
@@ -0,0 +1,7 @@
+# Sort by Last Char 7kyu
+# Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+# If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+# All inputs will be valid.
+def last(x)
+  x.split.sort_by { |word| word[-1] }
+end
