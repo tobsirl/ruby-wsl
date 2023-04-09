e2fc2b415a6796214cc4cd290792dc0aae9c9df8
Paul Tobin <tobster111@gmail.com>
2023-04-09 09:05:58 +0100

    Code

--- codewars/8kyu.rb
+++ codewars/8kyu.rb
@@ -6,3 +6,11 @@
 def uni_total(str)
   str.chars.map(&:ord).sum
 end
+
+# Sum of positive 8kyu
+# You get an array of numbers, return the sum of all of the positives ones.
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+def positive_sum(arr)
+  arr.select(&:positive?).sum
+end
+
