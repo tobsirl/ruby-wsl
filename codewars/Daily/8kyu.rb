04451d3a78314295c9c29bfca4cb8f50cc34a9ea
Paul Tobin <tobster111@gmail.com>
2023-04-12 18:10:41 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -42,3 +42,14 @@ class Person
   end
 end
 
+# Classy Extensions 8kyu
+# Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing classes
+# Task
+# Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+# The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+class Cat < Animal
+  def speak
+    "#{name} meows."
+  end
+end
+
