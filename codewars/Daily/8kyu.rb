b6b1b3dbd8292885ffd7f424237086facba8f431
Paul Tobin <tobster111@gmail.com>
2023-04-14 16:51:03 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -68,3 +68,23 @@ class Cube
     self.side = num
   end
 end
+
+# Grasshopper - Terminal Game #1 8kyu
+# In this first kata in the series, you need to define a Hero prototype to be used in a terminal game.
+# The hero should have the following attributes:
+# attribute value
+# name user argument or 'Hero'
+# position '00'
+# health 100
+# damage 5
+# experience 0
+class Hero
+  attr_accessor :name, :position, :health, :damage, :experience
+  def initialize(name = 'Hero')
+    @name = name
+    @position = '00'
+    @health = 100
+    @damage = 5
+    @experience = 0
+  end
+end
