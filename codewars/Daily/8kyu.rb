181ccc9339c255a4a0b11e6a9fac340f81b6c667
Paul Tobin <tobster111@gmail.com>
2023-04-15 09:26:26 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -88,3 +88,28 @@ class Hero
     @experience = 0
   end
 end
+
+# Finsih Guess the Number Game 8kyu
+# Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+# If the user tries to guess more than the limit, the function should throw an error
+# If the user guess is right it should return true
+# If the user guess is wrong it should return false and lose a life
+# Can you finish the game so all the rules are met?
+class Guesser
+  attr_accessor :number, :lives
+  def initialize(number, lives)
+    @number = number
+    @lives = lives
+  end
+
+  def guess(n)
+    raise 'No more guesses allowed' if lives.zero?
+    if n == number
+      true
+    else
+      self.lives -= 1
+      false
+    end  
+  end
+end
+
