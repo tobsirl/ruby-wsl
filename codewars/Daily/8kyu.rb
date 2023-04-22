3bfc9052f150859a3ff0bb3248dc467277a2a6a7
Paul Tobin <tobster111@gmail.com>
2023-04-22 09:16:22 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -159,3 +159,20 @@ end
 def apple(x)
   x.to_i**2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
 end
+
+# Grasshopper - Bug Squashing 8kyu
+# Terminal game bug squashing
+# You are creating a text-based terminal version of your favorite board game. 
+# In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+health = 100
+position = 0
+coins = 0
+
+def main ()
+  roll_dice()
+  move()
+  combat()
+  get_coins()
+  buy_health()
+  print_status()
+end
