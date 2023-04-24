638b540debe8705d35794db5469838116256816a
Paul Tobin <tobster111@gmail.com>
2023-04-24 18:30:19 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -195,3 +195,22 @@ def alias_gen(first_name, last_name)
     "Your name must start with a letter from A - Z."
   end
 end
+
+# Dashatize it 6kyu
+# Given a variable n,
+# If n is an integer, Return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
+# If n is not an integer, return an empty value.
+# Ex:
+# dashatize(274) -> '2-7-4'
+# dashatize(6815) -> '68-1-5'
+def dashatize(num)
+  return 'nil' unless num.is_a?(Integer)
+  num = num.to_s
+  num = num.gsub(/([13579])/, '-\1-')
+  num = num.split('-')
+  num.delete('')
+  num.join('-')
+end
+
+
+
