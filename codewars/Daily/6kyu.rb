daea47ce0b318d1fb18bcfc793f0117abefbe03f
Paul Tobin <tobster111@gmail.com>
2023-05-01 09:11:18 +0100

    #

--- codewars/Daily/6kyu.rb
+++ codewars/Daily/6kyu.rb
@@ -63,3 +63,12 @@ def decipher_this(string)
     gsub(/^(.)(.)(.*)(.)$/, '\1\4\3\2')
   end.join(' ')
 end
+
+# Reverse every other word in the string 6kyu
+# https://www.codewars.com/kata/58d76854024c72c3e20000de/train/ruby
+# Reverse every other word in a given string, then return the string.
+# Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word.
+# Punctuation marks should be treated as if they are a part of the word in this kata.
+def reverse_alternate(string)
+  string.split.map.with_index { |word, index| index.odd? ? word.reverse : word }.join(' ')
+end
