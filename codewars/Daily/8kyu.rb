4a8a7fb10006b1399556e691debdb98facc2c5f7
Paul Tobin <tobster111@gmail.com>
2023-05-03 09:34:31 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -232,3 +232,19 @@ end
 def fake_bin(s)
   s.gsub(/[0-4]/, '0').gsub(/[5-9]/, '1')
 end
+
+# Polish alphabet 8kyu
+# There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+# Your task is to change the letters with diacritics:
+# ą -> a,
+# ć -> c,
+# ę -> e,
+# ł -> l,
+# ń -> n,
+# ó -> o,
+# ś -> s,
+# ź -> z,
+# ż -> z
+def correct_polish_letters(s)
+  s.tr('ąćęłńóśźż', 'acelnoszz')
+end
