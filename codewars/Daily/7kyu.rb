eafa02a0069d6dd56542ccc3bb7c42e7f028e542
Paul Tobin <tobster111@gmail.com>
2023-05-05 08:59:07 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -86,3 +86,26 @@ end
 def spacify(str)
   str.chars.join(' ')
 end
+
+# Odd-Even String Sort 7kyu
+# https://www.codewars.com/kata/580755730b5a77650500010c/train/ruby
+# Given a string S. You have to return another string such that even-indexed and odd-indexed characters of S are grouped and groups are space-separated (see sample below)
+# Note:
+# 0 is considered to be an even index.
+# All input strings are valid with no spaces
+# input: 'CodeWars'
+# output 'CdWr oeas'
+# S[0] = 'C'
+# S[1] = 'o'
+# S[2] = 'd'
+# S[3] = 'e'
+# S[4] = 'W'
+# S[5] = 'a'
+# S[6] = 'r'
+# S[7] = 's'
+# Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+# odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+# And the final string to return is 'Cdwr oeas'
+def sort_my_string(s)
+  s.chars.partition.with_index { |_, i| i.even? }.map(&:join).join(' ')
+end
