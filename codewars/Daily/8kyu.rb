62aae2b7ac06383df75807c366e2bafcac2a198d
Paul Tobin <tobster111@gmail.com>
2023-05-07 09:08:10 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -258,3 +258,17 @@ end
 def duck_duck_goose(players, goose)
   players[(goose - 1) % players.length].name
 end
+
+# Array Leaders (Array Series #3) 7kyu
+# Definition
+# An element is leader if it is greater than The Sum all the elements to its right side.
+# Task
+# Given an array/list [] of integers , Find all the LEADERS in the array.
+# Notes
+# Array/list size is at least 3 .
+# Array/list's numbers Will be mixture of positives , negatives and zeros
+# Repetition of numbers in the array/list could occur.
+# Returned Array/list should store the leading numbers in the same order in the original array/list .
+def array_leaders(numbers)
+  numbers.select.with_index { |num, index| num > numbers[index + 1..-1].sum }
+end
