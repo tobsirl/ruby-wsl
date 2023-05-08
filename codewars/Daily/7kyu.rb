28a57822994137be3512578520f3db8da8a1b30a
Paul Tobin <tobster111@gmail.com>
2023-05-08 17:34:34 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -109,3 +109,13 @@ end
 def sort_my_string(s)
   s.chars.partition.with_index { |_, i| i.even? }.map(&:join).join(' ')
 end
+
+# Smallest value of an array 7kyu
+# https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/ruby
+# Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+# Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+# min([1,2,3,4,5], 'value') // => 1
+# min([1,2,3,4,5], 'index') // => 0
+def find_smallest(numbers, to_return)
+  to_return == 'value' ? numbers.min : numbers.index(numbers.min)
+end
