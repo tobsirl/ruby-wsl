00cecc5159e910754a4f23962f1900ef1cc7dd2d
Paul Tobin <tobster111@gmail.com>
2023-05-10 18:02:29 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -139,3 +139,17 @@ def calc(x)
   total2 = total1.to_s.gsub('7', '1').to_i
   total1.digits.sum - total2.digits.sum
 end
+
+# Maximum Length Difference 7kyu
+# https://www.codewars.com/kata/5663f5305102699bad000056/train/ruby
+# You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+# Find max(abs(length(x) − length(y)))
+# If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+# #Example:
+# # a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+# # a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+# # mxdiflg(a1, a2) --> 13
+def mxdiflg(a1, a2)
+  return -1 if a1.empty? || a2.empty?
+  [a1.map(&:length).max - a2.map(&:length).min, a2.map(&:length).max - a1.map(&:length).min].max
+end
