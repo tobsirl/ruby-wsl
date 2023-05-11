e8fe75604f9450cd5b97c56442af11f0d8a41a46
Paul Tobin <tobster111@gmail.com>
2023-05-11 18:13:27 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -153,3 +153,24 @@ def mxdiflg(a1, a2)
   return -1 if a1.empty? || a2.empty?
   [a1.map(&:length).max - a2.map(&:length).min, a2.map(&:length).max - a1.map(&:length).min].max
 end
+
+# Maximum Product 7kyu
+# https://www.codewars.com/kata/5a4138acf28b82aa43000117/train/ruby
+# Task
+# Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+# Notes
+# Array/list size is at least 2 .
+# Array/list numbers could be a mixture of positives , ngatives also zeroes .
+# Input >> Output Examples
+# adjacentElementsProduct([1, 2, 3]); ==> return 6  
+# Explanation:
+# The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+# adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+# Explanation:
+# Max product obtained from multiplying 5 * 10 = 50 .
+# adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])  ==>  return -14
+# Explanation:
+# The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent numbers in the array.
+def adjacent_element_product(array)
+  array.each_cons(2).map { |a, b| a * b }.max
+end
