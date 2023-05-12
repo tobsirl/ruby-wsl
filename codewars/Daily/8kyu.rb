5db70481b49f2aa4a5d95bbd29f4c5d6503d1fc0
Paul Tobin <tobster111@gmail.com>
2023-05-12 14:24:30 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -272,3 +272,14 @@ end
 def array_leaders(numbers)
   numbers.select.with_index { |num, index| num > numbers[index + 1..-1].sum }
 end
+
+# Find the Slope 8kyu
+# https://www.codewars.com/kata/55a75e2d0803fea18f00009d/train/ruby
+# Given an array of 4 integers
+# [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+# For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+# Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). Slope: https://en.wikipedia.org/wiki/Slope
+def find_slope(points)
+  return 'undefined' if points[2] - points[0] == 0
+  ((points[3] - points[1]) / (points[2] - points[0])).to_s
+end
