da48de7985e4925b6e499f8163e19d9ff301aaf9
Paul Tobin <tobster111@gmail.com>
2023-05-14 09:30:17 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -174,3 +174,14 @@ end
 def adjacent_element_product(array)
   array.each_cons(2).map { |a, b| a * b }.max
 end
+
+# Squares sequence 7kyu
+# https://www.codewars.com/kata/5546180ca783b6d2d5000062/train/ruby
+# Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. 
+# If n is negative or zero, return an empty array/list.
+# Examples
+# 2, 5  -->  [2, 4, 16, 256, 65536]
+# 3, 3  -->  [3, 9, 81]
+def squares(x, n)
+  n.times.map { |i| x**(2**i) }
+end
