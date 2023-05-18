7604645bd6cf1744f55e927ddf9f7889a2b018bc
Paul Tobin <tobster111@gmail.com>
2023-05-18 09:28:47 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -224,3 +224,13 @@ class Block
     2 * (@width * @length + @width * @height + @length * @height)
   end
 end
+
+# Help the Fruit Guy 7kyu
+# https://www.codewars.com/kata/557af4c6169ac832300000ba/train/ruby
+# Our fruit guy has a bag of fruits (array of strings) where some fruits are rotten. He wants to replace all the rotten fruits by good ones.
+# For example, given this array ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"].
+# Your task is to implement a method that will take as an argument an array of strings containing fruits and should return an array of strings where all the rotten fruits are replaced by good ones.
+# Note: if the array is null or empty you should return empty array ([]). the rotten fruit name will be in this format rottenFruitname where is the 1st letter of the fruit name is uppercase.
+def remove_rotten(fruit_basket)
+  fruit_basket&.map { |fruit| fruit.gsub('rotten', '').downcase } || []
+end
