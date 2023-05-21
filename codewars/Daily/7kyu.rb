44579d7271f379db00bf6ad6de6e303116a3e30f
Paul Tobin <tobster111@gmail.com>
2023-05-21 09:42:40 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -266,3 +266,10 @@ end
 def sort_gift_code(code)
   code.chars.sort.join
 end
+
+# Alternate case 7kyu
+# https://www.codewars.com/kata/57a62154cf1fa5b25200031e/train/ruby
+# Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+def alternate_case(string)
+  string.swapcase
+end
