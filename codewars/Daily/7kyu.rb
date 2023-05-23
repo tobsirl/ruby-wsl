390ca8ce0dfa879ba814f61d639222c820d3b016
Paul Tobin <tobster111@gmail.com>
2023-05-23 18:01:01 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -273,3 +273,16 @@ end
 def alternate_case(string)
   string.swapcase
 end
+
+# Build a square 7kyu
+# https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/ruby
+# I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 0 and 50.
+# Example
+# n = 3, so I expect a 3x3 square back just like below as a string:
+# +++
+# +++
+# +++
+def generate_shape(n)
+  result = ('+' * n + "\n") * n
+  result[0..-2]
+end
