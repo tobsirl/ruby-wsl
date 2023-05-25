a6ec46522b79d618886ee04e0f0d7d3a709b6630
Paul Tobin <tobster111@gmail.com>
2023-05-25 08:41:07 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -286,3 +286,30 @@ def generate_shape(n)
   result = ('+' * n + "\n") * n
   result[0..-2]
 end
+
+# Check the exam 7kyu
+# https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/ruby
+# The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"].
+# The second one contains a student's submitted answers.
+# The two arrays are not empty and are the same length. Return the score for this array of answers,
+# giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer,
+# represented as an empty string (in C the space character is used).
+# If the score < 0, return 0.
+# For example:
+# checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+# checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+# checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+# checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+def check_exam(arr1, arr2)
+  result = 0
+  arr1.each_with_index do |answer, index|
+    if answer == arr2[index]
+      result += 4
+    elsif arr2[index] == ''
+      result += 0
+    else
+      result -= 1
+    end
+  end
+  result < 0 ? 0 : result
+end
