131b4d514faafc9cf18a69174c7a9c768b7c79bc
Paul Tobin <tobster111@gmail.com>
2023-05-26 17:32:30 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -347,3 +347,15 @@ end
 def ensure_question(s)
   s.end_with?('?') ? s : s + '?'
 end
+
+# Pillars 8kyu
+# https://www.codewars.com/kata/5bb0c58f484fcd170700063d/train/ruby
+# There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same.
+# Your function accepts three arguments:
+# number of pillars (≥ 1);
+# distance between pillars (10 - 30 meters);
+# width of the pillar (10 - 50 centimeters).
+# Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+def pillars(num_of_pillars, dist, width)
+  num_of_pillars > 1 ? (num_of_pillars - 1) * dist * 100 + (num_of_pillars - 2) * width : 0
+end
