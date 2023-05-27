8ca5aca2fe484fdac1e77227b52e726ed4082e0a
Paul Tobin <tobster111@gmail.com>
2023-05-27 10:20:47 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -313,3 +313,18 @@ def check_exam(arr1, arr2)
   end
   result < 0 ? 0 : result
 end
+
+# The old switcheroo 7kyu
+# https://www.codewars.com/kata/55d410c492e6ed767000004f/train/ruby
+# Write a function
+# vowel_2_index
+# that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+# E.g:
+# vowel_2_index('this is my string') == 'th3s 6s my str15ng'
+# vowel_2_index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+# vowel_2_index('') == ''
+def vowel_2_index(string)
+  vowels = %w[a e i o u]
+  string.chars.map.with_index { |char, index| vowels.include?(char.downcase) ? index + 1 : char }.join
+end
+
