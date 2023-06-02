9bde72a52d748b92bfa741ec99da538e0c5917d0
Paul Tobin <tobster111@gmail.com>
2023-06-02 09:29:06 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -373,3 +373,21 @@ end
 def to_freud(sentence)
   sentence.split.map { |word| word.empty? ? "" : "sex" }.join(" ")
 end
+
+# Classic Hello World 8kyu
+# https://www.codewars.com/kata/57036f007fd72e3b77000023/train/ruby
+# You are given a method called main, make it print the line Hello World!, 
+# (yes, that includes a new line character at the end) and don't return anything
+# Note that for some languages, the function main is the entry point of the program.
+# Here's how it will be tested:
+# Solution.main("parameter1","parameter2","parametern");
+# Hints:
+# Check your references
+# Think about the scope of your method
+# For prolog you can use write but there are better ways
+class Solution
+  def self.main(*args)
+    puts "Hello World!"
+  end
+end
+
