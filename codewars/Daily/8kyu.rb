7eabc681184307e898d84d03754b751e009bdda7
Paul Tobin <tobster111@gmail.com>
2023-06-06 08:59:14 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -410,3 +410,19 @@ def validate_hello(greetings)
   greetings.match?(/(hello|ciao|salut|hallo|hola|ahoj|czesc)/i)
 end
 
+# UEFA EURO 2016 8kyu
+# https://www.codewars.com/kata/57613fb1033d766171000d60/train/ruby
+# Finish the uefaEuro2016() function so it return string just like in the examples below:
+# uefa_euro_2016(['Germany', 'Ukraine'],[2, 0]) # "At match Germany - Ukraine, Germany won!"
+# uefa_euro_2016(['Belgium', 'Italy'],[0, 2]) # "At match Belgium - Italy, Italy won!"
+# uefa_euro_2016(['Portugal', 'Iceland'],[1, 1]) # "At match Portugal - Iceland, teams played draw."
+def uefa_euro_2016(teams, scores)
+  if scores[0] > scores[1]
+    "At match #{teams[0]} - #{teams[1]}, #{teams[0]} won!"
+  elsif scores[0] < scores[1]
+    "At match #{teams[0]} - #{teams[1]}, #{teams[1]} won!"
+  else
+    "At match #{teams[0]} - #{teams[1]}, teams played draw."
+  end
+end
+
