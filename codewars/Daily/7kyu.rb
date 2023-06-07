b7881c70107502d064b5b50f1a708748b04e72a6
Paul Tobin <tobster111@gmail.com>
2023-06-07 09:22:04 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -477,3 +477,40 @@ def seven(m)
   [m, steps]
 end
   
+# Jumping Number (Special Numbers Series #4) 7kyu
+# https://www.codewars.com/kata/5a54e796b3bfa8932c0000ed/train/ruby
+# Definition
+# Jumping number is the number that All adjacent digits in it differ by 1.
+# Task
+# Given a number, Find if it is Jumping or not .
+# Warm-up (Highly recommended)
+# Playing With Numbers Series
+# Notes
+# Number passed is always Positive .
+# Return the result as String .
+# The difference between ‘9’ and ‘0’ is not considered as 1 .
+# All single digit numbers are considered as Jumping numbers.
+# Input >> Output Examples
+# jumpingNumber(9) ==> return "Jumping!!"
+# Explanation:
+# It's single-digit number
+# jumpingNumber(79) ==> return "Not!!"
+# Explanation:
+# Adjacent digits don't differ by 1
+# jumpingNumber(23) ==> return "Jumping!!"
+# Explanation:
+# Adjacent digits differ by 1
+# jumpingNumber(556847) ==> return "Not!!"
+# Explanation:
+# Adjacent digits don't differ by 1
+# jumpingNumber(4343456) ==> return "Jumping!!"
+# Explanation:
+# Adjacent digits differ by 1
+def jumping_number(n)
+  n = n.to_s.chars.map(&:to_i)
+  n.each_with_index do |num, i|
+    next if i == n.length - 1
+    return 'Not!!' if (num - n[i + 1]).abs != 1
+  end
+  'Jumping!!'
+end
