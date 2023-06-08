19fb01a9dc11bddfc56411a801eb7a0bc04a1241
Paul Tobin <tobster111@gmail.com>
2023-06-08 09:23:38 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -514,3 +514,12 @@ def jumping_number(n)
   end
   'Jumping!!'
 end
+
+# The Office III - Broken Photocopier 7kyu
+# https://www.codewars.com/kata/57ed56657b45ef922300002b/train/ruby
+# The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+# Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+# Given a string of binary, return the version the photocopier gives you as a string.
+def broken(x)
+  x.tr('01', '10')
+end
