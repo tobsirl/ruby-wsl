c5f5e0fbccb3792abd6e078c4325a847f4d4ec05
Paul Tobin <tobster111@gmail.com>
2023-06-09 09:02:54 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -523,3 +523,12 @@ end
 def broken(x)
   x.tr('01', '10')
 end
+
+# Simple string characters 7kyu
+# https://www.codewars.com/kata/5a29a0898f27f2d9c9000058/train/ruby
+# In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters, as follows.
+# Solve("*'&ABCDabcde12345") = [4,5,5,3].
+# --the order is: uppercase letters, lowercase, numbers and special characters.
+def solve(s)
+  [s.count('A-Z'), s.count('a-z'), s.count('0-9'), s.count('^a-zA-Z0-9')]
+end
