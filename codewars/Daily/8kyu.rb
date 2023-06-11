53f41fff27804dfd5651e2a5fa0c271fde8bd5a2
Paul Tobin <tobster111@gmail.com>
2023-06-11 09:22:04 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -426,3 +426,17 @@ def uefa_euro_2016(teams, scores)
   end
 end
 
+# Is your period late? 8kyu
+# https://www.codewars.com/kata/578a8a01e9fd1549e50001f1/train/ruby
+# In this kata, we will make a function to test whether a period is late.
+# Our function will take three parameters:
+# last - The Date object with the date of the last period
+# today - The Date object with the date of the check
+# cycleLength - Integer representing the length of the cycle in days
+# If the today is later from last than the cycleLength, the function should return true.
+# We consider it to be late if the number of passed days is larger than the cycleLength.
+# Otherwise return false.
+def period_is_late(last,today,cycle_length)
+  (today - last).to_i > cycle_length
+end
+
