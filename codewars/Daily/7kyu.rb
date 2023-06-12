252279b60a3ecfd6ff16c3abaa9603d7687f7627
Paul Tobin <tobster111@gmail.com>
2023-06-12 18:13:06 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -549,3 +549,27 @@ class Array
     integer_list.reject { |num| values_list.include?(num) }
   end
 end
+
+# Product Of Maximums Of Array (Array Series #2) 7kyu
+# https://www.codewars.com/kata/5a63948acadebff56f000018/train/ruby
+# Introduction and Warm-up (Highly recommended)
+# Playing With Lists/Arrays Series
+# Task
+# Given an array/list [] of integers , Find the product of the k maximal numbers.
+# Notes
+# Array/list size is at least 3 .
+# Array/list's numbers Will be mixture of positives , negatives and zeros
+# Repetition of numbers in the array/list could occur.
+# Input >> Output Examples
+# maxProduct ({4, 3, 5}, 2) ==>  return (20)
+# Explanation:
+# Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+# maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+# Explanation:
+# Since the size (k) equal 3 , then the subsequence of size 2 whose gives product of maxima is 8 * 9 * 10 = 720 .
+# maxProduct ({10, 8, 3, 2, 1, 4, 10}, 5) ==> return (9600)
+# Explanation:
+# Since the size (k) equal 5 , then the subsequence of size 2 whose gives product of maxima is 10 * 10 * 8 * 4 * 3 = 9600 .
+def max_product(numbers, size)
+  numbers.sort.last(size).reduce(:*)
+end
