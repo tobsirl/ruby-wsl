ced3bd3a0ed80790f1014f9de6e834dafc870d49
Paul Tobin <tobster111@gmail.com>
2023-06-17 09:27:43 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -484,3 +484,13 @@ def who_is_paying(name)
   name.length <= 2 ? [name] : [name, name[0..1]]
 end
 
+# Name on billboard 8kyu
+# https://www.codewars.com/kata/570e8ec4127ad143660001fd/train/ruby
+# You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30,
+# but that can be different if you are given 2 parameters instead of 1.
+# You can not use multiplier "*" operator.
+# If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).
+def billboard(name, price = 30)
+  name.chars.map { price }.sum
+end
+
