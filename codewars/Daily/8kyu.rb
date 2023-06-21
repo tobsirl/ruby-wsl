f00107a84f4e3beba974a1225aba3defd7a6c905
Paul Tobin <tobster111@gmail.com>
2023-06-21 18:06:15 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -494,3 +494,16 @@ def billboard(name, price = 30)
   name.chars.map { price }.sum
 end
 
+# Quadratic Coefficients Solver 8kyu
+# https://www.codewars.com/kata/5d59576768ba810001f1f8d6/train/ruby
+# In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+# Equation will be the form of ax^2 + bx + c = 0
+# Return type is a Vector containing coefficients of the equations in the order (a, b, c).
+# Since there are infinitely many solutions to this problem, we fix a = 1.
+# Remember, the roots can be written like (x-x1) * (x-x2) = 0
+# Example
+# quadratic(1,2) = (1, -3, 2)
+# This means (x-1) * (x-2) = 0; when we do the multiplication this becomes x^2 - 3x + 2 = 0
+def quadratic(x1, x2)
+  [1, -(x1 + x2), x1 * x2]
+end
