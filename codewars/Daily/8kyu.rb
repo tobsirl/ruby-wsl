7d28df3a9b2506483460571da3133f66aa721546
Paul Tobin <tobster111@gmail.com>
2023-06-23 16:43:10 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -512,3 +512,17 @@ end
 def _if(bool, ifTrue, ifFalse)
   bool ? ifTrue.call : ifFalse.call  
 end
+
+# Return Two Highest Values in List 8kyu
+# https://www.codewars.com/kata/57ab3c09bbf1b96c010000bf/train/ruby
+# In this kata, your job is to return the two highest values in a list, this doesn't include duplicates.
+# When given an empty list, you should also return an empty list, no strings will be passed into the list.
+# The return should also be ordered from highest to lowest.
+# If the argument passed isn't a list, you should return false.
+# Examples:
+# two_highest([4, 10, 10, 9]) should return [10, 9]
+# two_highest([]) should return []
+# two_highest("test") should return False
+def two_highest(list)
+  list.is_a?(Array) ? list.uniq.max(2) : false
+end
