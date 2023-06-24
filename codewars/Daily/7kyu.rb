d12f93b54c862a3a204f1f6186628ea4ca45f037
Paul Tobin <tobster111@gmail.com>
2023-06-24 09:26:02 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -666,3 +666,16 @@ end
 def solve(s)
   s.scan(/[aeiou]+/).map(&:length).max
 end
+
+# Incrementer 7kyu
+# https://www.codewars.com/kata/590e03aef55cab099a0002e8/train/ruby
+# Given an input of an array of digits num, return the array with each digit incremented by its position in the array. For example, the first digit will be incremented by 1, the second digit by 2 etc. Make sure to start counting your positions from 1 and not 0.
+# incrementer([1,2,3]) => [2,4,6]
+# Your result can only contain single digit numbers, so if adding a digit with it's position gives you a multiple-digit number, only the last digit of the number should be returned
+# incrementer([4,6,9,1,3]) => [5,8,2,5,8]
+#   - 9 + 3 (position of 9 in array) = 12
+#   - Only its last digit 2 should be returned
+# Lastly, return [] if your array is empty! Arrays will only contain numbers so don't worry about checking that.
+def incrementer(nums)
+  nums.map.with_index { |num, index| (num + index + 1).to_s[-1].to_i }
+end
