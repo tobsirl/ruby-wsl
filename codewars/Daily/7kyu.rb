a3e91683ef11e7afce4f0c39e6d5af98719bcc5b
Paul Tobin <tobster111@gmail.com>
2023-06-27 09:27:54 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -679,3 +679,19 @@ end
 def incrementer(nums)
   nums.map.with_index { |num, index| (num + index + 1).to_s[-1].to_i }
 end
+
+# Split In Parts 7kyu
+# https://www.codewars.com/kata/5650ab06d11d675371000003/train/ruby
+# The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+# Example:
+# Split the below string into other strings of size #3
+# 'supercalifragilisticexpialidocious'
+# Will return a new string
+# 'sup erc ali fra gil ist ice xpi ali doc iou s'
+# Assumptions:
+# String length is always greater than 0
+# String has no spaces
+# Size is always positive
+def split_in_parts(s, part_length)
+  s.chars.each_slice(part_length).map(&:join).join(' ')
+end
