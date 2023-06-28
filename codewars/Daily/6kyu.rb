970836f83d4a98f928355985f20931526587600f
Paul Tobin <tobster111@gmail.com>
2023-06-28 17:23:33 +0100

    Code

--- codewars/Daily/6kyu.rb
+++ codewars/Daily/6kyu.rb
@@ -104,4 +104,24 @@ def multiplication_table(size)
   (1..size).map { |num| (1..size).map { |n| n * num } }
 end
 
-
+# Reverse or rotate? 6kyu
+# https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/ruby
+# The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string)
+# of size sz (ignore the last chunk if its size is less than sz).
+# If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk;
+# otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+# If
+# sz is <= 0 or if str is empty return ""
+# sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+# Examples:
+# revrot("123456987654", 6) --> "234561876549"
+# revrot("123456987653", 6) --> "234561356789"
+# revrot("66443875", 4) --> "44668753"
+# revrot("66443875", 8) --> "64438756"
+# revrot("664438769", 8) --> "67834466"
+def revrot(str, sz)
+  return '' if sz <= 0 || sz > str.size
+  str.scan(/.{#{sz}}/).map do |chunk|
+    chunk.chars.map(&:to_i).sum % 2 == 0 ? chunk.reverse : chunk[1..-1] + chunk[0]
+  end.join
+end
