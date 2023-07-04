5d5b269bf59631143fa48698c62a1b1efac7f54f
Paul Tobin <tobster111@gmail.com>
2023-07-04 18:36:32 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -726,3 +726,13 @@ end
 def lost_sheep(friday, saturday, total)
   total - friday.sum - saturday.sum
 end
+
+# Largest Elements 7kyu
+# https://www.codewars.com/kata/53d32bea2f2a21f666000256/train/ruby
+# Write a program that outputs the top n elements from a list.
+# Example:
+# largest(2, [7,6,5,4,3,2,1])
+# # => [6,7]
+def largest(n, xs)
+  xs.sort.reverse.take(n).reverse
+end
