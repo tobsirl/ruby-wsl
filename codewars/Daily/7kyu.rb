e1c33ca1d5399e33302dfd3eefd3c10f79e52ea1
Paul Tobin <tobster111@gmail.com>
2023-07-05 17:11:27 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -736,3 +736,12 @@ end
 def largest(n, xs)
   xs.sort.reverse.take(n).reverse
 end
+
+# Evens and Odds 7kyu
+# https://www.codewars.com/kata/583ade15666df5a64e000058/train/ruby
+# This kata is about converting numbers to their binary or hexadecimal representation:
+# If a number is even, convert it to binary.
+# If a number is odd, convert it to hex.
+def evens_and_odds(n)
+  n.even? ? n.to_s(2) : n.to_s(16)
+end
