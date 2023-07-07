dbaf51e4a636fa6de78c68213bd162d9e8926358
Paul Tobin <tobster111@gmail.com>
2023-07-07 15:42:02 +0100

    Code

--- codewars/Daily/6kyu.rb
+++ codewars/Daily/6kyu.rb
@@ -145,3 +145,41 @@ def count(node, data)
   return 0 if node.nil?
   (node.data == data ? 1 : 0) + count(node.next, data)
 end
+
+# Array Helpers 6kyu
+# https://www.codewars.com/kata/525d50d2037b7acd6e000534/train/ruby
+# This kata is designed to test your ability to extend the functionality of built-in ruby classes.
+# In this case, we want you to extend the built-in Array class with the following methods:
+# square(), cube(), average(), sum(), even() and odd().
+# Explanation:
+# square() must return a copy of the array, containing all values squared, the original array must not be changed
+# cube() must return a copy of the array, containing all values cubed, the original array must not be changed
+# average() must return the average of all array values, average() on an empty array must return NaN
+# sum() must return the sum of all array values
+# even() must return an array of all even numbers, the original array must not be changed
+# odd() must return an array of all odd numbers, the original array must not be changed
+class Array
+  def square
+    map { |num| num**2 }
+  end
+
+  def cube
+    map { |num| num**3 }
+  end
+
+  def average
+    sum / size.to_f
+  end
+
+  def sum
+    reduce(:+)
+  end
+
+  def even
+    select(&:even?)
+  end
+
+  def odd
+    select(&:odd?)
+  end
+end
