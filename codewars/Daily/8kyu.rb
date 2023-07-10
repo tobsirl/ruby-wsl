785f4cbb7f3ad46b3c79aabae0b73b035a8de337
Paul Tobin <tobster111@gmail.com>
2023-07-10 15:41:33 +0100

    Code

--- codewars/Daily/8kyu.rb
+++ codewars/Daily/8kyu.rb
@@ -564,3 +564,17 @@ def year_days(year)
     "#{year} has 365 days"
   end
 end
+
+# Safen User Input Part I - htmlspecialchars 8kyu
+# https://www.codewars.com/kata/56bcaedfcf6b7f2125001118/train/ruby
+# You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more) which contains form fields so visitors can send emails or leave a comment on your website with ease. However, with ease comes danger. Every now and then, a hacker visits your website and attempts to compromise it through the use of XSS (Cross Site Scripting). This is done by injecting script tags into the website through form fields which may contain malicious code (e.g. a redirection to a malicious website that steals personal information).
+# Mission
+# Your mission is to implement a function htmlspecialchars() that converts the following potentially harmful characters:
+# < --> &lt;
+# > --> &gt;
+# " --> &quot;
+# & --> &amp;
+# Good luck :D
+def html_special_chars(s)
+  s.gsub(/[<>"&]/, '<' => '&lt;', '>' => '&gt;', '"' => '&quot;', '&' => '&amp;')
+end
