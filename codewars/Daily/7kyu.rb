b593e8866229767223f634cfbbf57488d9ace53b
Paul Tobin <tobster111@gmail.com>
2023-07-15 09:31:54 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -828,3 +828,13 @@ end
 def flatten(array)
   array.flatten(1)
 end
+
+# Find the lucky numbers 7kyu
+# https://www.codewars.com/kata/580435ab150cca22650001fb/train/ruby
+# Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+# For example,
+# filter_lucky([1,2,3,4,5,6,7,68,69,70,15,17]) => [7,70,17]
+# Don't worry about bad input, you will always receive a finite list of integers.
+def filter_lucky(arr)
+  arr.select { |num| num.to_s.include?('7') }
+end
