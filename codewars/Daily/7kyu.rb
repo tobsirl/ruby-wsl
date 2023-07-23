7675c8a7f2d7d1e55a8c1f1e47a80a675ee3954e
Paul Tobin <tobster111@gmail.com>
2023-07-23 09:43:47 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -875,3 +875,20 @@ end
 def band_name_generator(name)
   name[0] == name[-1] ? name.capitalize + name[1..-1] : 'The ' + name.capitalize
 end
+
+# Maximum Gap (Array Series #4)
+# https://www.codewars.com/kata/5a7893ef0025e9eb50000013/train/ruby
+# Task
+# Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+# Notes
+# Array/list size is at least 3 .
+# Array/list's numbers Will be mixture of positives and negatives also zeros_
+# Repetition of numbers in the array/list could occur.
+# The Maximum Gap is computed Regardless the sign.
+# Input >> Output Examples
+# maxGap ({13,10,5,2,9}) ==> return (4)
+# Explanation:
+# The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+def max_gap(numbers)
+  numbers.sort.each_cons(2).map { |a, b| b - a }.max
+end
