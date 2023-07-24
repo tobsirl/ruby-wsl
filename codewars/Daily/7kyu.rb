a5fae43f6c1f8785a3d97764b887accbdb17ff0c
Paul Tobin <tobster111@gmail.com>
2023-07-24 17:34:30 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -892,3 +892,27 @@ end
 def max_gap(numbers)
   numbers.sort.each_cons(2).map { |a, b| b - a }.max
 end
+
+# Deodorant Evaporator 7kyu
+# https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/train/ruby
+# This program tests the life of an evaporator containing a gas.
+# We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day)
+# and the threshold (threshold) in percentage beyond which the evaporator is no longer useful.
+# All numbers are strictly positive.
+# The program reports the nth day (as an integer) on which the evaporator will be out of use.
+# Example:
+# evaporator(10, 10, 5) -> 29
+# Note:
+# Content is in fact not necessary in the body of the function "evaporator",
+# you can use it or not use it, as you wish. Some people might prefer to reason with content,
+# some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+def evaporator(content, evap_per_day, threshold)
+  threshold = content * threshold / 100.0
+  days = 0
+  until content <= threshold
+    content -= content * evap_per_day / 100.0
+    days += 1
+  end
+  days
+end
+
