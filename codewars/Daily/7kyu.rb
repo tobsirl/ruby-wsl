ef501a03ec2be08c67fce9bab528b702bc6387b1
Paul Tobin <tobster111@gmail.com>
2023-07-29 09:30:47 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -950,3 +950,16 @@ def shorter_reverse_longer(a, b)
   a.length >= b.length ? b + a.reverse + b : a + b.reverse + a
 end
 
+# V A P O R C O D E 7kyu
+# https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/ruby
+# ASC Week 1 Challenge 4 (Medium #1)
+# Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E 
+# sentence converts all the letters into uppercase, and adds 2 spaces between each letter 
+# (or special character) to create this V A P O R W A V E effect.
+# Note that spaces should be ignored in this case.
+# Examples
+#  vaporcode("Lets go to the movies") # => "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+# vaporcode("Why isn't my code working?") # => "W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G"
+def vaporcode(string)
+  string.upcase.gsub(/\s+/, '').chars.join('  ')
+end
