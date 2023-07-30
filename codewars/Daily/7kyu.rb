50a072bcb3b9ef5855460e69bd542d5a758e0712
Paul Tobin <tobster111@gmail.com>
2023-07-30 09:28:06 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -963,3 +963,15 @@ end
 def vaporcode(string)
   string.upcase.gsub(/\s+/, '').chars.join('  ')
 end
+
+# Cat and Mouse - Easy Version 7kyu
+# https://www.codewars.com/kata/57ee24e17b45eff6d6000164/train/ruby
+# You will be given a string (x) featuring a cat 'C' and a mouse 'm'.
+# The rest of the string will be made up of '.'.
+# You need to find out if the cat can catch the mouse from it's current position.
+# The cat can jump over three characters. So:
+# C.....m returns 'Escaped!' <-- more than three characters between
+# C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.
+def cat_mouse(x)
+  x.length > 5 ? 'Escaped!' : 'Caught!'
+end
