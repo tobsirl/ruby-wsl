e37c1ae3b2e31430bd9f44018aa19d15003b762a
Paul Tobin <tobster111@gmail.com>
2023-07-31 18:22:17 +0100

    Code

--- codewars/Daily/6kyu.rb
+++ codewars/Daily/6kyu.rb
@@ -218,3 +218,27 @@ def beggars(values, n)
   end
   result
 end
+
+# extract file name 6kyu
+# https://www.codewars.com/kata/597770e98b4b340e5b000071/train/ruby
+# You have to extract a portion of the file name as follows:
+# Assume it will start with date represented as long number
+# Followed by an underscore
+# Youll have then a filename with an extension
+# it will always have an extra extension at the end
+# Inputs:
+# 1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION
+# 1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34
+# 1231231223123131_myFile.tar.gz2
+# Outputs
+# FILE_NAME.EXTENSION
+# This_is_an_otherExample.mpg
+# myFile.tar
+# Acceptable characters for random tests:
+# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-0123456789
+# The recommend way to solve it is using RegEx and specifically groups.
+class FileNameExtractor
+  def extract_file_name(dirty_file_name)
+    dirty_file_name[/^\d+_([^.]+\.[^.]+)/, 1]
+  end
+end
