1ec591b0501635dd85aecb0f555c048a4fd8bd03
Paul Tobin <tobster111@gmail.com>
2023-08-01 17:43:57 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -975,3 +975,19 @@ end
 def cat_mouse(x)
   x.length > 5 ? 'Escaped!' : 'Caught!'
 end
+
+# By 3, or not by 3? That is the question . . . 7kyu
+# https://www.codewars.com/kata/59f7fc109f0e86d705000043/train/ruby
+# A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three.
+# If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+# Given a series of numbers as a string, determine if the number represented by the string is divisible by three.
+# You can expect all test case arguments to be strings representing values greater than 0.
+# Example:
+# '123' // true
+# '8409' // true
+# '100853' // false
+# '33333333' // true
+# '7' // false
+def divisible_by_three(str)
+  str.chars.map(&:to_i).sum % 3 == 0
+end
