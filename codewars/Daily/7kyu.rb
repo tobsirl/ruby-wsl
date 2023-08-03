a48e88a96328f5f96ee3a59793298ee615f1ce40
Paul Tobin <tobster111@gmail.com>
2023-08-03 19:00:23 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -991,3 +991,23 @@ end
 def divisible_by_three(str)
   str.chars.map(&:to_i).sum % 3 == 0
 end
+
+# Valid Spacing 7kyu
+# https://www.codewars.com/kata/5f77d62851f6bc0033616bd8/train/ruby
+# Task
+# Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing.
+# The function should return either True or False.
+# For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces.
+# Below are some examples of what the function should return.
+# 'Hello world' = true
+# ' Hello world' = false
+# 'Hello world  ' = false
+# 'Hello  world' = false
+# 'Hello' = true
+# Even though there are no spaces, it is still valid because none are needed
+# 'Helloworld' = true
+# ' ' = false
+# '' = true
+def valid_spacing(s)
+  s == s.split(' ').join(' ')
+end
