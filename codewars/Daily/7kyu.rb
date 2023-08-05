8ebdb9bf0ae466f51b2186af1837ea83155a67e1
Paul Tobin <tobster111@gmail.com>
2023-08-05 09:34:58 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1018,3 +1018,13 @@ end
 def get_min_max(seq)
   [seq.min, seq.max]
 end
+
+# Stanton measure 7kyu
+# https://www.codewars.com/kata/59a1cdde9f922b83ee00003b/train/ruby
+# The Stanton measure of an array is computed as follows: count the number of 1s in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+# Write a function which takes an integer array and returns its Stanton measure.
+# Example:
+# The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+def stanton_measure(arr)
+  arr.count(arr.count(1))
+end
