d816c6c8f8c33dfee2d00934ede5d2d9e6947c68
Paul Tobin <tobster111@gmail.com>
2023-08-07 09:36:15 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1028,3 +1028,18 @@ end
 def stanton_measure(arr)
   arr.count(arr.count(1))
 end
+
+# String Merge! 7kyu
+# https://www.codewars.com/kata/597bb84522bc93b71e00007e/train/ruby
+# Given two words and a letter, return a single word that's a combination of both words, 
+# merged at the point where the given letter first appears in each word. 
+# The returned word should have the beginning of the first word and the ending of the second, 
+# with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+# Examples
+# StringMerge("hello", "world", "l")       ==>  "held"
+# StringMerge("coding", "anywhere", "n")   ==>  "codinywhere"
+# StringMerge("jason", "samson", "s")      ==>  "jasamson"
+# StringMerge("wonderful", "people", "e")  ==>  "wondeople"
+def string_merge(word1, word2, letter)
+  word1[0...word1.index(letter)] + word2[word2.index(letter)..-1]
+end
