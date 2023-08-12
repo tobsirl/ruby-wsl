794fa7e651bdfb5157e5ee1a981b4358a4301764
Paul Tobin <tobster111@gmail.com>
2023-08-12 09:29:14 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1098,3 +1098,16 @@ end
 def oper(fct, s)
   fct.call(s)
 end
+
+# Unlucky Days 7kyu
+# https://www.codewars.com/kata/56eb0be52caf798c630013c0/train/ruby
+# Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+# Find the number of Friday 13th in the given year.
+# Input: Year as an integer.
+# Output: Number of Black Fridays in the year as an integer.
+# Examples:
+# unluckyDays(2015) == 3
+# unluckyDays(1986) == 1
+def unlucky_days(year)
+  (1..12).count { |month| Time.new(year, month, 13).friday? }
+end
