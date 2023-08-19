40970593ea3c11c29722a53d4eede8424f719bc5
Paul Tobin <tobster111@gmail.com>
2023-08-19 09:46:39 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1135,3 +1135,11 @@ end
 def most_frequent_item_count(collection)
   collection.group_by(&:itself).values.map(&:size).max || 0
 end
+
+# Sum of integers in string 7kyu
+# https://www.codewars.com/kata/598f76a44f613e0e0b000026/train/ruby
+# Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+# Note: only positive integers will be tested.
+def sum_of_integers_in_string(s)
+  s.scan(/\d+/).map(&:to_i).sum
+end
