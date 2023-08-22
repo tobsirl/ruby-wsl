57cfd15375dee7e66ea2b6281ac46d0e3f99784d
Paul Tobin <tobster111@gmail.com>
2023-08-22 17:31:58 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1166,3 +1166,15 @@ end
 def make_string(s)
   s.split.map { |word| word[0] }.join
 end
+
+# Drone Fly-By 7kyu
+# https://www.codewars.com/kata/58356a94f8358058f30004b5/train/ruby
+# The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+# In this kata we will recreate that stunt... sort of.
+# You will be given two strings: lamps and drone. Lamps represents a row of lamps, currently off, each represented by 'x'. When these lamps are on, they should be represented by 'o'.
+# The drone string represents the position of the drone 'T' (any better suggestion for character??) and its flight path up until this point '='. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+# Return the resulting lamps string. See example tests for more clarity.
+# FUNDAMENTALS
+def fly_by(lamps, drone)
+  lamps[0...drone.length].gsub('x', 'o') + lamps[drone.length..-1]
+end
