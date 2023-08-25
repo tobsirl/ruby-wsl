2edcbba5f93bb132011d2b58669764902181ebe2
Paul Tobin <tobster111@gmail.com>
2023-08-25 16:01:21 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1192,3 +1192,19 @@ end
 def is_nice(arr)
   arr.all? { |num| arr.include?(num - 1) || arr.include?(num + 1) } && arr.length > 0
 end
+
+# Simple consecutive pairs 7kyu
+# https://www.codewars.com/kata/5a3e1319b6486ac96f000049/train/ruby
+# In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+# pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+# The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+# --the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+# --the second pair is (5,8) and are not consecutive
+# --the third pair is (-4,-3), consecutive. Count = 2
+# --the fourth pair is (7,6), also consecutive. Count = 3.
+# --the last digit has no pair, so we ignore.
+# More examples in the test cases.
+# Good luck!
+def pairs(arr)
+  arr.each_slice(2).count { |a, b| a && b && (a - b).abs == 1 }
+end
