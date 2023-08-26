df5520e742bb9c6bd3aa406ed5149414324dc2a1
Paul Tobin <tobster111@gmail.com>
2023-08-26 10:14:49 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1208,3 +1208,14 @@ end
 def pairs(arr)
   arr.each_slice(2).count { |a, b| a && b && (a - b).abs == 1 }
 end
+
+# Arithmetic progression 7kyu
+# https://www.codewars.com/kata/55caf1fd8063ddfa8e000018/train/ruby
+# In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+# The result should be a string of numbers, separated by comma and space.
+# Example
+# # first element: 1, difference: 2, how many: 5
+# arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+def arithmetic_sequence_elements(a, d, n)
+  (1..n).map { |i| a + (i - 1) * d }.join(', ')
+end
