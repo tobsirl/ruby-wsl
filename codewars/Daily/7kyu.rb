651fc090e323cdbdc2e7ea411b24eca387dcbea8
Paul Tobin <tobster111@gmail.com>
2023-08-27 09:57:49 +0100

    Code

--- codewars/Daily/7kyu.rb
+++ codewars/Daily/7kyu.rb
@@ -1219,3 +1219,27 @@ end
 def arithmetic_sequence_elements(a, d, n)
   (1..n).map { |i| a + (i - 1) * d }.join(', ')
 end
+
+# Minimum Steps (Array Series #6) 7kyu
+# https://www.codewars.com/kata/5a91a7c5fd8c061367000002/train/ruby
+# Task
+# Given an array of N integers, you have to find how many times you have to add up the smallest numbers in the array until their Sum becomes greater or equal to K.
+# Notes:
+# List size is at least 3.
+# All numbers will be positive.
+# Numbers could occur more than once , (Duplications may exist).
+# Threshold K will always be reachable.
+# Input >> Output Examples
+# minimumSteps({1, 10, 12, 9, 2, 3}, 6)  ==>  return (2)
+# Explanation:
+# We add two smallest elements (1 + 2), their sum is 3 .
+# Then we add the next smallest number to it (3 + 3) , so the sum becomes 6 .
+# Now the result is greater or equal to 6 , Hence the output is (2) i.e (2) operations are required to do this .
+def minimum_steps(numbers, value)
+  numbers.sort!
+  sum = 0
+  numbers.each_with_index do |num, i|
+    sum += num
+    return i if sum >= value
+  end
+end
